f79787660f314f378fdc32c57f2cab2737006dd9
George Babey <gbabey@edx.org>
2018-07-27 09:06:10 -0400

    Added cookie tests for login/logout

--- src/data/actions/authentication.test.js
+++ src/data/actions/authentication.test.js
@@ -2,17 +2,28 @@ import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 import axios from 'axios';
 import MockAdapter from 'axios-mock-adapter';
+import Cookies from 'universal-cookie';
 
-import { login } from './authentication';
+import { login, logout } from './authentication';
 
 import {
   FETCH_LOGIN_FAILURE,
   FETCH_LOGIN_REQUEST,
   FETCH_LOGIN_SUCCESS,
+  LOGOUT,
 } from '../constants/authentication';
 
 const mockStore = configureMockStore([thunk]);
 const axiosMock = new MockAdapter(axios);
+
+jest.genMockFromModule('universal-cookie');
+jest.mock('universal-cookie');
+
+const mockCookies = {
+  set: jest.fn(),
+  remove: jest.fn(),
+};
+Cookies.mockImplementation(() => mockCookies);
 
 describe('actions', () => {
   afterEach(() => {
@@ -45,6 +56,25 @@ describe('actions', () => {
       });
     });
 
+    it('sets the action_token after successful login request', () => {
+      const responseData = {
+        access_token: 'random_access_token_data',
+      };
+
+      axiosMock.onPost('http://localhost:18000/oauth2/access_token')
+        .replyOnce(200, JSON.stringify(responseData));
+
+      const store = mockStore();
+
+      return store.dispatch(login(email, password)).then(() => {
+        expect(mockCookies.set).toHaveBeenCalledWith(
+          'access_token',
+          responseData.access_token,
+          { secure: true },
+        );
+      });
+    });
+
     it('dispatches failure action after failed login request', () => {
       const expectedActions = [
         { type: FETCH_LOGIN_REQUEST },
@@ -58,7 +88,19 @@ describe('actions', () => {
         expect(store.getActions()).toEqual(expectedActions);
       });
     });
+  });
 
-    // TODO: test logout action
+  describe('logout', () => {
+    it('dispatches logout action', () => {
+      const store = mockStore();
+      store.dispatch(logout());
+      expect(store.getActions()).toEqual([{ type: LOGOUT }]);
+    });
+
+    it('removes the access_token cookie', () => {
+      const store = mockStore();
+      store.dispatch(logout());
+      expect(mockCookies.remove).toHaveBeenCalled();
+    });
   });
 });
