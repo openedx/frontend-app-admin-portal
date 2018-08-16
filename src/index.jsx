4a0813667832d02347b411634e8c4b9c0221073c
George Babey <gbabey@edx.org>
2018-08-16 12:01:28 -0400

    ENT-1179. Fix react router / redux to dispatch LOCATION_CHANGE events

--- src/index.jsx
+++ src/index.jsx
@@ -2,13 +2,11 @@ import 'babel-polyfill'; // general ES2015 polyfill (e.g. promise)
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {
-  BrowserRouter as Router,
   Route,
   Switch,
 } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'react-router-redux';
-import createHistory from 'history/createBrowserHistory';
 import { Helmet } from 'react-helmet';
 
 import EnterpriseApp from './containers/EnterpriseApp';
@@ -21,32 +19,29 @@ import EnterpriseIndexPage from './containers/EnterpriseIndexPage';
 import PrivateRoute from './containers/PrivateRoute';
 import LogoutHandler from './containers/LogoutHandler';
 import store from './data/store';
+import history from './data/history';
 import './index.scss';
-
-const history = createHistory();
 
 const AppWrapper = () => (
   <Provider store={store}>
     <ConnectedRouter history={history}>
-      <Router>
-        <div>
-          <Helmet
-            titleTemplate="%s - edX Portal"
-            defaultTitle="edX Portal"
-          />
-          <Header />
-          <Switch>
-            <Route exact path="/login" component={LoginPage} />
-            <Route exact path="/logout" component={LogoutHandler} />
-            <Route exact path="/support" component={SupportPage} />
-            <PrivateRoute exact path="/enterprises" component={EnterpriseIndexPage} />
-            <PrivateRoute path="/:enterpriseSlug" component={EnterpriseApp} />
-            <PrivateRoute exact path="/" component={EnterpriseIndexPage} />
-            <Route component={NotFoundPage} />
-          </Switch>
-          <Footer />
-        </div>
-      </Router>
+      <div>
+        <Helmet
+          titleTemplate="%s - edX Portal"
+          defaultTitle="edX Portal"
+        />
+        <Header />
+        <Switch>
+          <Route exact path="/login" component={LoginPage} />
+          <Route exact path="/logout" component={LogoutHandler} />
+          <Route exact path="/support" component={SupportPage} />
+          <PrivateRoute exact path="/enterprises" component={EnterpriseIndexPage} />
+          <PrivateRoute path="/:enterpriseSlug" component={EnterpriseApp} />
+          <PrivateRoute exact path="/" component={EnterpriseIndexPage} />
+          <Route component={NotFoundPage} />
+        </Switch>
+        <Footer />
+      </div>
     </ConnectedRouter>
   </Provider>
 );
