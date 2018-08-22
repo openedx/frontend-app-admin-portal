2e78e119443ace3bb2c6cad1af69a3dc86d2ad97
George Babey <gbabey@edx.org>
2018-08-22 09:31:09 -0400

    Adds support for sorting of demo data
    This adds support for sorting to the demoDataLoader.js script. Also a small change to only pull
    in the english locale for faker, as that library is quite large

--- src/demo/rewireEnterpriseDataApiService.test.js
+++ src/demo/rewireEnterpriseDataApiService.test.js
@@ -12,15 +12,50 @@ describe('rewireEnterpriseDataApiService', () => {
     EnterpriseDataApiService.fetchDashboardAnalytics = fetchDashboardAnalytics;
   });
 
-  it('rewires fetchCourseEnrollments call', () => {
+  describe('fetchCourseEnrollments', () => {
+    it('rewires fetchCourseEnrollments call', () => {
+      rewire();
+      return EnterpriseDataApiService.fetchCourseEnrollments('test-enterprise-id', {}).then((results) => {
+        // Only testing for data types, not actual values
+        const expectedResults = {
+          count: expect.any(Number),
+          num_pages: expect.any(Number),
+          current_page: expect.any(Number),
+          results: expect.any(Array),
+        };
+        expect(results.data).toEqual(expectedResults);
+      });
+    });
+    it('supports sorting via options', () => {
+      rewire();
+      return EnterpriseDataApiService.fetchCourseEnrollments('test-enterprise-id', { ordering: 'current_grade' }).then((results) => {
+        let previous = results.data.results[0];
+        results.data.results.forEach((enrollment) => {
+          expect(enrollment.current_grade).toBeGreaterThanOrEqual(previous.current_grade);
+          previous = enrollment;
+        });
+      });
+    });
+    it('supports pagination via options', () => {
+      rewire();
+      return EnterpriseDataApiService.fetchCourseEnrollments('test-enterprise-id', { page: 2 }).then((results) => {
+        expect(results.data.current_page).toEqual(2);
+      });
+    });
+  });
+
+  describe('fetchDashboardAnalytics', () => {
     rewire();
-    return EnterpriseDataApiService.fetchCourseEnrollments('test-enterprise-id', {}).then((results) => {
-      // Only testing for data types, not actual values
+    return EnterpriseDataApiService.fetchDashboardAnalytics().then((results) => {
       const expectedResults = {
-        count: expect.any(Number),
-        num_pages: expect.any(Number),
-        current_page: expect.any(Number),
-        results: expect.any(Array),
+        active_learners: {
+          past_month: expect.any(Number),
+          past_week: expect.any(Number),
+        },
+        course_completions: expect.any(Number),
+        enrolled_learners: expect.any(Number),
+        last_update_date: expect.any(Date),
+        number_of_users: expect.any(Number),
       };
       expect(results.data).toEqual(expectedResults);
     });
