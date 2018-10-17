c97941b16a4abeb933a3e46610ace7f1d1abacfa
Adam Stankiewicz <agstanki@gmail.com>
2018-10-17 07:41:18 -0400

    Fixing unresolved promise rejection (#88)
    * Fixing unresolved promise rejection
    
    * Clean up tests

--- src/demo/rewireEnterpriseDataApiService.test.js
+++ src/demo/rewireEnterpriseDataApiService.test.js
@@ -12,6 +12,21 @@ describe('rewireEnterpriseDataApiService', () => {
     fetchDashboardAnalytics,
   } = EnterpriseDataApiService;
 
+  const verifyFetchMethod = (fetchMethod, options = {}) => fetchMethod(options).then((results) => {
+    // Only testing for data types, not actual values
+    const expectedResults = {
+      count: expect.any(Number),
+      num_pages: expect.any(Number),
+      current_page: expect.any(Number),
+      results: expect.any(Array),
+    };
+    expect(results.data).toEqual(expectedResults);
+  });
+
+  beforeEach(() => {
+    rewire();
+  });
+
   afterEach(() => {
     // rewire in the tests overrides the methods on EnterpriseDataApiService so we restore
     // them to their original method calls after each test. Ideally we would just save/restore
@@ -25,76 +40,31 @@ describe('rewireEnterpriseDataApiService', () => {
     EnterpriseDataApiService.fetchDashboardAnalytics = fetchDashboardAnalytics;
   });
 
-  describe('fetchEnrollments', () => {
-    const verifyFetchMethodEnrollments = (fetchMethod, options = {}) => fetchMethod(options).then((results) => { // eslint-disable-line max-len
-      // Only testing for data types, not actual values
-      const expectedResults = {
-        count: expect.any(Number),
-        num_pages: expect.any(Number),
-        current_page: expect.any(Number),
-        results: expect.any(Array),
-      };
-      expect(results.data).toEqual(expectedResults);
-    });
-    it('rewires fetchCourseEnrollments call', () => {
-      rewire();
-      return verifyFetchMethodEnrollments(EnterpriseDataApiService.fetchCourseEnrollments);
-    });
-    it('rewires fetchCourseEnrollments with options call', () => {
-      rewire();
-      return verifyFetchMethodEnrollments(
-        EnterpriseDataApiService.fetchCourseEnrollments,
-        {
-          learner_activity: 'active_past_week',
-        },
-      );
-    });
-    it('rewires fetchEnrolledLearners call', () => {
-      rewire();
-      return verifyFetchMethodEnrollments(EnterpriseDataApiService.fetchEnrolledLearners);
-    });
-    it('rewires filterEnrolledLearnersForInactiveCourses call', () => {
-      rewire();
-      return verifyFetchMethodEnrollments(EnterpriseDataApiService.fetchEnrolledLearnersForInactiveCourses); // eslint-disable-line max-len
-    });
-    it('rewires fetchUnenrolledRegisteredLearners call', () => {
-      rewire();
-      return verifyFetchMethodEnrollments(EnterpriseDataApiService.fetchUnenrolledRegisteredLearners); // eslint-disable-line max-len
-    });
-    it('rewires fetchCompletedLearners call', () => {
-      rewire();
-      return verifyFetchMethodEnrollments(EnterpriseDataApiService.fetchCompletedLearners);
-    });
-    it('supports sorting via options', () => {
-      rewire();
-      return EnterpriseDataApiService.fetchCourseEnrollments({ ordering: 'current_grade' }).then((results) => {
-        let previous = results.data.results[0];
-        results.data.results.forEach((enrollment) => {
-          expect(enrollment.current_grade).toBeGreaterThanOrEqual(previous.current_grade);
-          previous = enrollment;
-        });
-      });
-    });
-    it('supports pagination via options', () => {
-      rewire();
-      return EnterpriseDataApiService.fetchCourseEnrollments({ page: 2 }).then((results) => {
-        expect(results.data.current_page).toEqual(2);
-      });
-    });
-  });
+  it('rewires fetchCourseEnrollments call', () => verifyFetchMethod(EnterpriseDataApiService.fetchCourseEnrollments));
 
-  describe('fetchCourseEnrollmentsCsv', () => {
-    it('rewires fetchCourseEnrollmentsCsv call', () => {
-      rewire();
-      return EnterpriseDataApiService.fetchCourseEnrollmentsCsv().then((results) => {
-        expect(results.data).toEqual(expect.any(String));
-      });
-    });
-  });
+  it('rewires fetchCourseEnrollments with options call', () =>
+    verifyFetchMethod(
+      EnterpriseDataApiService.fetchCourseEnrollments,
+      {
+        learner_activity: 'active_past_week',
+      },
+    ));
 
-  describe('fetchDashboardAnalytics', () => {
-    rewire();
-    return EnterpriseDataApiService.fetchDashboardAnalytics().then((results) => {
+  it('rewires fetchEnrolledLearners call', () => verifyFetchMethod(EnterpriseDataApiService.fetchEnrolledLearners));
+
+  it('rewires filterEnrolledLearnersForInactiveCourses call', () => verifyFetchMethod(EnterpriseDataApiService.fetchEnrolledLearnersForInactiveCourses));
+
+  it('rewires fetchUnenrolledRegisteredLearners call', () => verifyFetchMethod(EnterpriseDataApiService.fetchUnenrolledRegisteredLearners));
+
+  it('rewires fetchCompletedLearners call', () => verifyFetchMethod(EnterpriseDataApiService.fetchCompletedLearners));
+
+  it('rewires fetchCourseEnrollmentsCsv call', () =>
+    EnterpriseDataApiService.fetchCourseEnrollmentsCsv().then((results) => {
+      expect(results.data).toEqual(expect.any(String));
+    }));
+
+  it('rewires fetchDashboardAnalytics', () =>
+    EnterpriseDataApiService.fetchDashboardAnalytics().then((results) => {
       const expectedResults = {
         active_learners: {
           past_month: expect.any(Number),
@@ -106,6 +76,19 @@ describe('rewireEnterpriseDataApiService', () => {
         number_of_users: expect.any(Number),
       };
       expect(results.data).toEqual(expectedResults);
-    });
-  });
+    }));
+
+  it('supports sorting via options', () =>
+    EnterpriseDataApiService.fetchCourseEnrollments({ ordering: 'current_grade' }).then((results) => {
+      let previous = results.data.results[0];
+      results.data.results.forEach((enrollment) => {
+        expect(enrollment.current_grade).toBeGreaterThanOrEqual(previous.current_grade);
+        previous = enrollment;
+      });
+    }));
+
+  it('supports pagination via options', () =>
+    EnterpriseDataApiService.fetchCourseEnrollments({ page: 2 }).then((results) => {
+      expect(results.data.current_page).toEqual(2);
+    }));
 });
