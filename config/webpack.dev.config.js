e8019b3370b943ddff553a2190b721254b53e9c3
Douglas Hall <dhall@edx.org>
2018-11-14 14:48:39 -0500

    Fix login redirect.

--- config/webpack.dev.config.js
+++ config/webpack.dev.config.js
@@ -111,7 +111,7 @@ module.exports = Merge.smart(commonConfig, {
       ECOMMERCE_API_BASE_URL: null,
       SEGMENT_KEY: null,
       ACCESS_TOKEN_COOKIE_NAME: 'edx-jwt-cookie-header-payload',
-      CSRF_COOKIE_NAME: 'csrftoken',
+      USER_INFO_COOKIE_NAME: 'edx-user-info',
       FEATURE_FLAGS: {
         CODE_MANAGEMENT: true,
       },
