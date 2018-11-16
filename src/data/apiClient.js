d3874f89cf23419e75dccd002dd16c6a5f0b3219
Douglas Hall <douglas_hall@icloud.com>
2018-11-16 18:03:45 -0500

    Set up for CSRF token management.

--- src/data/apiClient.js
+++ src/data/apiClient.js
@@ -4,6 +4,7 @@ const apiClient = getAuthenticatedAPIClient({
   appBaseUrl: process.env.BASE_URL,
   loginUrl: process.env.LOGIN_URL,
   logoutUrl: process.env.LOGOUT_URL,
+  csrfTokenApiPath: process.env.CSRF_TOKEN_API_PATH,
   refreshAccessTokenEndpoint: process.env.REFRESH_ACCESS_TOKEN_ENDPOINT,
   accessTokenCookieName: process.env.ACCESS_TOKEN_COOKIE_NAME,
   userInfoCookieName: process.env.USER_INFO_COOKIE_NAME,
