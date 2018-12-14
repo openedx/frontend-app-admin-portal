0c51b953699c0e61de7c0d5ee46c8c6f31356c39
Christopher Pappas <cpappas@edx.org>
2018-12-14 13:54:24 -0500

    Adding new reducer to the list of rest of reducers

--- src/data/reducers/index.js
+++ src/data/reducers/index.js
@@ -9,6 +9,7 @@ import csv from './csv';
 import userProfile from './userProfile';
 import coupons from './coupons';
 import sidebar from './sidebar';
+import codeAssignmentReducer from './codeAssignment';
 
 const identityReducer = (state) => {
   const newState = { ...state };
@@ -28,6 +29,7 @@ const rootReducer = combineReducers({
   userProfile,
   coupons,
   sidebar,
+  codeAssignmentReducer,
 });
 
 export default rootReducer;
