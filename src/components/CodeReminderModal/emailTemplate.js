9dff04ffc279c2ed94aa72205a4d9a9af83b845b
Adam Stankiewicz <agstanki@gmail.com>
2019-02-06 16:08:37 -0500

    Update email template

--- src/components/CodeReminderModal/emailTemplate.js
+++ src/components/CodeReminderModal/emailTemplate.js
@@ -1,11 +1,11 @@
-const emailTemplate = `This is a reminder email that your learning manager has provided you with an access code to take a course at edX. You have redeemed this code {REDEEMED_OFFER_COUNT} time(s) out of {TOTAL_OFFER_COUNT} number of available course redemptions.
+const emailTemplate = `This is a reminder that your Learning Manager has provided you with an access code to take a course at edX. You have redeemed this code {REDEEMED_OFFER_COUNT} time(s) out of {TOTAL_OFFER_COUNT} number of available course redemptions.
 
 edX Login: {USER_EMAIL}
 Enrollment URL: {ENROLLMENT_URL}
 Access Code: {CODE}
 Expiration Date: {EXPIRATION_DATE}
 
-You may go directly to the Enrollment URL to view courses that are available for this code or you can insert the access code at checkout under “coupon code” for applicable courses.
+You may go directly to the Enrollment URL to view courses that are available for this code or you can insert the access code at checkout under "coupon code" for applicable courses.
 
 For any questions, please reach out to your Learning Manager.`;
 
