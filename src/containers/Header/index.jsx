c5dedc20259fa03ac3852d964d0683cbb6b1ab27
Adam Stankiewicz <agstanki@gmail.com>
2019-04-11 14:55:50 -0400

    Fix for null profileImage value

--- src/containers/Header/index.jsx
+++ src/containers/Header/index.jsx
@@ -4,15 +4,18 @@ import { fetchUserAccount, UserAccountApiService } from '@edx/frontend-auth';
 import apiClient from '../../data/apiClient';
 import Header from '../../components/Header';
 
-const mapStateToProps = state => ({
-  enterpriseName: state.portalConfiguration.enterpriseName,
-  enterpriseSlug: state.portalConfiguration.enterpriseSlug,
-  enterpriseLogo: state.portalConfiguration.enterpriseLogo,
-  email: state.userAccount.email,
-  username: state.authentication.username,
-  userProfileImageUrl: state.userAccount.profileImage.imageUrlMedium,
-  hasSidebarToggle: state.sidebar.hasSidebarToggle,
-});
+const mapStateToProps = (state) => {
+  const { userAccount: { profileImage } } = state;
+  return {
+    enterpriseName: state.portalConfiguration.enterpriseName,
+    enterpriseSlug: state.portalConfiguration.enterpriseSlug,
+    enterpriseLogo: state.portalConfiguration.enterpriseLogo,
+    email: state.userAccount.email,
+    username: state.authentication.username,
+    userProfileImageUrl: profileImage && profileImage.imageUrlMedium,
+    hasSidebarToggle: state.sidebar.hasSidebarToggle,
+  };
+};
 
 const mapDispatchToProps = (dispatch) => {
   const userAccountApiService = new UserAccountApiService(apiClient, process.env.LMS_BASE_URL);
