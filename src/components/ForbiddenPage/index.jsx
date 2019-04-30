db77afe09a45228862a78a92a3bbd77abc783748
Mushtaq Ali <mushtaque@edx.org>
2019-04-30 18:31:33 +0500

    Add a 403 message component - ENT-1753

---
+++ src/components/ForbiddenPage/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Helmet from 'react-helmet';
+import { MailtoLink } from '@edx/paragon';
+
+import H1 from '../../components/H1';
+
+const ForbiddenPage = () => (
+  <div className="container-fluid mt-3">
+    <Helmet>
+      <title>Access Denied</title>
+    </Helmet>
+    <div className="text-center py-5">
+      <H1>403</H1>
+      <p className="lead">You do not have access to this page.</p>
+      <p>
+        For assistance, please contact edX Enterprise Support at <MailtoLink to="customersuccess@edx.org" content=" customersuccess@edx.org" />.
+      </p>
+    </div>
+  </div>
+);
+
+export default ForbiddenPage;
