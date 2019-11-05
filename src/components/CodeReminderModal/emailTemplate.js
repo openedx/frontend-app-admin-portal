80b2a23a3c9342245f6b19c6f5f1fb93e7ce969c
Justin Hynes <jhynes@edx.org>
2019-11-05 12:20:28 -0500

    ENT-2373 | Improve enterprise code email template messages
    [ENT-2373]
    - Adjust wording within the code assignment, reminder, and revocation email templates

--- src/components/CodeReminderModal/emailTemplate.js
+++ src/components/CodeReminderModal/emailTemplate.js
@@ -1,16 +1,15 @@
 const emailTemplate = {
   greeting: `This is a reminder that your Learning Manager has provided you with an access code to take a course at edX.
-  `,
-  body: `You have redeemed this code {REDEEMED_OFFER_COUNT} time(s) out of {TOTAL_OFFER_COUNT} number of available course redemptions.
+`,
+  body: `You have redeemed this code {REDEEMED_OFFER_COUNT} time(s) out of {TOTAL_OFFER_COUNT} available course redemptions.
 
 edX Login: {USER_EMAIL}
 Access Code: {CODE}
-Expiration Date: {EXPIRATION_DATE}`,
-  closing: `
-You can insert the access code at checkout under "coupon code" for applicable courses.
-
-For any questions, please reach out to your Learning Manager.
+Expiration Date: {EXPIRATION_DATE}
 `,
+  closing: `You can insert the access code at checkout under "coupon code" for applicable courses.
+
+For any questions, please reach out to your Learning Manager.`,
 };
 
 export default emailTemplate;
