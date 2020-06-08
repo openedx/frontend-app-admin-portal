66e5719daf8cb638b24e1bb80dfff9b30c22ddfb
Adam Stankiewicz <agstanki@gmail.com>
2020-06-07 22:02:35 -0400

    make revoking users functional and refresh data after revoke

--- src/components/subscriptions/data/service.js
+++ src/components/subscriptions/data/service.js
@@ -1,29 +1,66 @@
 import faker from 'faker';
 import moment from 'moment';
 
-import { PAGE_SIZE } from '../constants';
+import { PAGE_SIZE, ACTIVE, ASSIGNED, DEACTIVATED } from '../constants';
 
-function createUser(licenseStatus) {
+export function createSampleUser(licenseStatus) {
   return {
-    uuid: faker.random.uuid(),
+    userId: faker.random.uuid(),
     emailAddress: faker.internet.email(),
     licenseStatus,
   };
 }
 
 const users = [
-  [...Array(6)].map(() => createUser('active')),
-  [...Array(3)].map(() => createUser('assigned')),
-  [...Array(1)].map(() => createUser('deactivated')),
+  [...Array(6)].map(() => createSampleUser(ACTIVE)),
+  [...Array(3)].map(() => createSampleUser(ASSIGNED)),
+  [...Array(1)].map(() => createSampleUser(DEACTIVATED)),
 ].flat();
 
 const getUsersByStatus = ({ status, list }) => list.filter(user => user.licenseStatus === status);
 
 function getAllocatedLicensesCount() {
-  const licensedUsers = getUsersByStatus({ status: 'active', list: users });
-  const pendingUsers = getUsersByStatus({ status: 'assigned', list: users });
+  const licensedUsers = getUsersByStatus({ status: ACTIVE, list: users });
+  const pendingUsers = getUsersByStatus({ status: ASSIGNED, list: users });
   return licensedUsers.length + pendingUsers.length;
 }
+
+function createNewUser({ userId, emailAddress, licenseStatus = ASSIGNED }) {
+  const user = { userId, emailAddress, licenseStatus };
+  users.append(user);
+  return user;
+}
+
+function updateUserLicenseStatus({ userId, status }) {
+  const index = users.findIndex(item => item.userId === userId);
+
+  if (index !== -1) {
+    users[index] = {
+      ...users[index],
+      licenseStatus: status,
+    };
+
+    return users[index];
+  }
+
+  return null;
+}
+
+const purchaseDate = moment(faker.date.past());
+const startDate = moment(purchaseDate).add(15, 'days');
+const endDate = moment(startDate).add(6, 'months');
+const numLicensedPurchased = faker.random.number({ min: 300, max: 1000 });
+
+const getSubscriptionDetails = () => ({
+  uuid: faker.random.uuid(),
+  purchaseDate: purchaseDate.toISOString(),
+  startDate: startDate.toISOString(),
+  endDate: endDate.toISOString(),
+  licenses: {
+    allocated: getAllocatedLicensesCount(),
+    available: numLicensedPurchased,
+  },
+});
 
 /**
  * This function mocks out the response from a non-existant API endpoint. Once the endpoint
@@ -31,20 +68,9 @@ function getAllocatedLicensesCount() {
  * call to get this data.
  */
 export function fetchSubscriptionDetails() {
-  const purchaseDate = moment(faker.date.past());
-  const startDate = moment(purchaseDate).add(15, 'days');
-  const endDate = moment(startDate).add(6, 'months');
-
-  return Promise.resolve({
-    uuid: faker.random.uuid(),
-    purchaseDate: purchaseDate.toISOString(),
-    startDate: startDate.toISOString(),
-    endDate: endDate.toISOString(),
-    licenses: {
-      allocated: getAllocatedLicensesCount(),
-      available: faker.random.number({ min: 300, max: 1000 }),
-    },
-  });
+  const details = getSubscriptionDetails();
+  return Promise.resolve(details);
+  // return Promise.reject(new Error('Could not connect to the server'));
 }
 
 /**
@@ -67,7 +93,8 @@ export function fetchSubscriptionUsersOverview(options = {}) {
     deactivated: getUsersByStatus({ status: 'deactivated', list: userList }).length,
   };
 
-  return response;
+  return Promise.resolve(response);
+  // return Promise.reject(new Error('Could not connect to the server'));
 }
 
 /**
@@ -86,7 +113,6 @@ export function fetchSubscriptionUsers(options = {}) {
   if (searchQuery) {
     response.results = response.results.filter(user => user.emailAddress === searchQuery);
   }
-
   if (statusFilter) {
     response.results = response.results.filter(user => user.licenseStatus === statusFilter);
   }
@@ -94,7 +120,10 @@ export function fetchSubscriptionUsers(options = {}) {
   response.count = response.results.length;
   response.results = response.results.slice(0, PAGE_SIZE);
 
+  console.log('fetchSubscriptionUsers...', options, response);
+
   return Promise.resolve(response);
+  // return Promise.reject(new Error('Could not connect to the server'));
 }
 
 /**
@@ -122,6 +151,24 @@ export function addLicensesForUsers(payload) {
  * call to get this data.
  */
 export function sendLicenseRevoke(options = {}) {
-  return Promise.resolve(options);
+  const { userId } = options;
+
+  updateUserLicenseStatus({ userId, status: DEACTIVATED });
+
+  const updatedUser = users.find(item => item.userId === userId);
+  return Promise.resolve(updatedUser);
+  // return Promise.reject(new Error('Could not connect to the server'));
+}
+
+/**
+ * This function mocks out the response from a non-existant API endpoint. Once the endpoint
+ * exists, the contents of this function will use the `apiClient` to make an actual API
+ * call to get this data.
+ */
+export function addUsers(options = {}) {
+  const { users: newUsers } = options;
+  const created = newUsers.map(user => createNewUser(user));
+
+  return Promise.resolve(created);
   // return Promise.reject(new Error('Could not connect to the server'));
 }
