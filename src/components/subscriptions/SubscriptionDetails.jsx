79543b168733e2be962adbc755badfff4704ee13
Brandon Baker <bbaker@edx.org>
2020-09-14 14:56:01 -0400

    Remove purchase date info
    This field no longer exists on the Subscription model so don't display
    anything
    
    ENT-3409

--- src/components/subscriptions/SubscriptionDetails.jsx
+++ src/components/subscriptions/SubscriptionDetails.jsx
@@ -12,14 +12,6 @@ export default function SubscriptionDetails() {
       <div className="mt-3 d-flex align-items-center">
         <div className="mr-5">
           <div className="text-uppercase text-muted">
-            <small>Purchase Date</small>
-          </div>
-          <div className="lead">
-            {formatTimestamp({ timestamp: details.purchaseDate })}
-          </div>
-        </div>
-        <div className="mr-5">
-          <div className="text-uppercase text-muted">
             <small>Start Date</small>
           </div>
           <div className="lead">
