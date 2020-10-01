da40e9980c3ba8c6d33f907437fa0fe179954d6b
Alex Dusenbery <adusenbery@edx.org>
2020-10-01 13:14:56 -0400

    Revert "fix: attempt to refresh JWT if expired"
    This reverts commit 41b86f72ecbb6b5bc43f56c29a56aafcd6ab67aa.

--- config/webpack.dev.config.js
+++ config/webpack.dev.config.js
@@ -107,7 +107,7 @@ module.exports = Merge.smart(commonConfig, {
       LOGIN_URL: 'http://localhost:18000/login',
       LOGOUT_URL: 'http://localhost:18000/logout',
       CSRF_TOKEN_API_PATH: '/csrf/api/v1/token',
-      REFRESH_ACCESS_TOKEN_ENDPOINT: 'http://localhost:18000/login_refresh',
+      REFRESH_ACCESS_TOKEN_ENDPOINT: 'http://localhost:18000/login',
       DATA_API_BASE_URL: 'http://localhost:8000',
       ECOMMERCE_BASE_URL: 'http://localhost:18130',
       LICENSE_MANAGER_BASE_URL: 'http://localhost:18170',
