6cc4bd98ef0532ff52a7b8b2d4855c5420c3be39
Adam Stankiewicz <astankiewicz@edx.org>
2020-12-11 15:00:51 -0500

    make bootstrap column sizes dynamic (#414)
    * demo columns

--- src/components/subscriptions/SubscriptionManagementPage.jsx
+++ src/components/subscriptions/SubscriptionManagementPage.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { Helmet } from 'react-helmet';
-import { Route } from 'react-router';
+import { Route, Switch } from 'react-router-dom';
+import { Container } from '@edx/paragon';
 
 import Hero from '../Hero';
 import SubscriptionData from './SubscriptionData';
@@ -17,20 +18,20 @@ function SubscriptionManagementPage({ enterpriseId }) {
       <Helmet title={PAGE_TITLE} />
       <Hero title={PAGE_TITLE} />
       <main role="main" className="manage-subscription">
-        <div className="container-fluid">
-          <Route
-            key="subscription-list"
-            exact
-            path="/:enterpriseSlug/admin/subscriptions"
-            component={MultipleSubscriptionsPage}
-          />
-          <Route
-            key="subscription-detail"
-            exact
-            path="/:enterpriseSlug/admin/subscriptions/:subscriptionUUID"
-            component={SubscriptionDetailPage}
-          />
-        </div>
+        <Container className="py-3" fluid>
+          <Switch>
+            <Route
+              path="/:enterpriseSlug/admin/subscriptions"
+              component={MultipleSubscriptionsPage}
+              exact
+            />
+            <Route
+              path="/:enterpriseSlug/admin/subscriptions/:subscriptionUUID"
+              component={SubscriptionDetailPage}
+              exact
+            />
+          </Switch>
+        </Container>
       </main>
     </SubscriptionData>
   );
