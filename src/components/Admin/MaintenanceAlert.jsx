ec0f88f7281b204b37c10ef9bcb4d5b982b7132b
Adam Stankiewicz <agstanki@gmail.com>
2020-12-14 16:56:20 -0500

    add temporary maintenance alert

---
+++ src/components/Admin/MaintenanceAlert.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Alert } from '@edx/paragon';
+
+export default () => (
+  <Alert variant="warning" className="mt-3">
+    Learner Progress Report will be down for planned maintenance on Wednesday, Dec 16th from 10am-12pm EST
+  </Alert>
+);
