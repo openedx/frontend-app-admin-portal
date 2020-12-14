c2126441ec285e15313ce79b75af4bde4c5557d4
Adam Stankiewicz <agstanki@gmail.com>
2020-12-14 17:05:23 -0500

    change to full month

--- src/components/Admin/MaintenanceAlert.jsx
+++ src/components/Admin/MaintenanceAlert.jsx
@@ -3,6 +3,6 @@ import { Alert } from '@edx/paragon';
 
 export default () => (
   <Alert variant="warning" className="mt-3">
-    Learner Progress Report will be down for planned maintenance on Wednesday, Dec 16th from 10am-12pm EST
+    Learner Progress Report will be down for planned maintenance on Wednesday, December 16th from 10am-12pm EST
   </Alert>
 );
