eca0d436076ca4b75c930e2083e57095786ab155
Adam Stankiewicz <agstanki@gmail.com>
2020-12-16 11:43:20 -0500

    just change the alert messaging for now

---
+++ src/components/Admin/MaintenanceAlert.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Alert } from '@edx/paragon';
+
+import { configuration } from '../../config';
+
+export default () => {
+  if (!configuration?.SHOW_LEARNER_REPORT_MAINTENANCE_ALERT) {
+    return null;
+  }
+
+  return (
+    <Alert variant="warning" className="mt-3">
+      Learner Progress Report will be down for planned maintenance on Thursday, December 17th from 10am-12pm EST
+    </Alert>
+  );
+};
