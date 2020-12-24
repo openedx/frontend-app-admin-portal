6f89c38ceca751fa8f6b9cd91259dfdab5d5e061
Zaman Afzal <zamanafzal@gmail.com>
2020-12-24 15:23:12 +0500

    Revert "Revert "ENT-3671 Keep email field populated while switching in between new and old templates""

--- src/containers/TemplateSourceFields/index.jsx
+++ src/containers/TemplateSourceFields/index.jsx
@@ -2,7 +2,7 @@ import { connect } from 'react-redux';
 
 import TemplateSourceFields from '../../components/TemplateSourceFields';
 
-import fetchEmailTemplates, { setEmailTemplateSource, currentFromTemplate } from '../../data/actions/emailTemplate';
+import fetchEmailTemplates, { setEmailTemplateSource, currentFromTemplate, setEmailAddress } from '../../data/actions/emailTemplate';
 
 const mapStateToProps = state => ({
   emailTemplateSource: state.emailTemplate.emailTemplateSource,
@@ -11,6 +11,7 @@ const mapStateToProps = state => ({
 
 const mapDispatchToProps = dispatch => ({
   setEmailTemplateSource: templateSource => dispatch(setEmailTemplateSource(templateSource)),
+  setEmailAddress: (emailAddress, emailType) => dispatch(setEmailAddress(emailAddress, emailType)),
   currentFromTemplate: (type, template) => dispatch(currentFromTemplate(type, template)),
   fetchEmailTemplates: (options) => {
     dispatch(fetchEmailTemplates(options));
