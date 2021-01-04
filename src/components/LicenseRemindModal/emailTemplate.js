3790febc414446e401cbd717f79ce0e18bdc0804
Brandon Baker <bbaker6225@users.noreply.github.com>
2021-01-04 16:18:36 -0500

    fix: fix quote that was being escaped (#426)

--- src/components/LicenseRemindModal/emailTemplate.js
+++ src/components/LicenseRemindModal/emailTemplate.js
@@ -1,7 +1,7 @@
 import { getSubscriptionContactText } from '../../utils';
 
 const emailTemplate = {
-  greeting: "We noticed you haven't had a chance to start learning on edX!  It’s easy to get started and browse the course catalog.",
+  greeting: 'We noticed you haven’t had a chance to start learning on edX!  It’s easy to get started and browse the course catalog.',
   body: `{ENTERPRISE_NAME} partnered with edX to give everyone access to high-quality online courses. Start your subscription and browse courses in nearly every subject including Data Analytics, Digital Media, Business & Leadership, Communications, Computer Science and so much more. Courses are taught by experts from the world’s leading universities and corporations.
 
 Start learning: {LICENSE_ACTIVATION_LINK}
