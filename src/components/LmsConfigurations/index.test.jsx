e6bade1143e9b5816f6c7726c2e934ad602f4eb4
Talia <trhodes@edx.org>
2021-01-05 10:54:43 -0500

    Add Success Factor to LMS Configurations

--- src/components/LmsConfigurations/index.test.jsx
+++ src/components/LmsConfigurations/index.test.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { mount } from 'enzyme';
 import { REQUIRED_MOODLE_CONFIG_FIELDS } from './MoodleIntegrationConfigForm';
 import { REQUIRED_CANVAS_CONFIG_FIELDS } from './CanvasIntegrationConfigForm';
+import { REQUIRED_SUCCESS_FACTOR_CONFIG_FIELDS } from './SuccessFactorsIntegrationConfigForm';
 import LmsConfigurations from './index';
 import LmsApiService from '../../data/services/LmsApiService';
 import { REQUIRED_BLACKBOARD_CONFIG_FIELDS } from './BlackboardIntegrationConfigForm';
@@ -11,10 +12,22 @@ jest.mock('./../../data/services/LmsApiService');
 const moodleResponse = {
   data: { results: [{}] },
 };
+const successFactorsResponse = {
+  data: { results: [{}] },
+};
+
 const notFoundMoodleResponse = {
   request: { status: 404, statusText: 'Not Found' },
 };
+const notFoundSuccessFactorsResponse = {
+  request: { status: 404, statusText: 'Not Found' },
+};
+
 const badMoodleResponse = {
+  request: { status: 400, statusText: 'Bad Request' },
+};
+
+const badSuccessFactorsResponse = {
   request: { status: 400, statusText: 'Bad Request' },
 };
 
@@ -23,6 +36,10 @@ const canvasResponse = {
 };
 const notFoundCanvasResponse = {
   request: { status: 404, statusText: 'Not Found' },
+};
+
+const badCanvasResponse = {
+  request: { status: 400, statustext: 'Bad Request' },
 };
 
 const blackboardResponse = {
@@ -35,32 +52,44 @@ const badBlackboardResponse = {
   request: { status: 400, statusText: 'Bad Request' },
 };
 
-const formData = new FormData();
-
 REQUIRED_MOODLE_CONFIG_FIELDS.forEach((field) => {
-  formData.append(field, 'testdata');
   moodleResponse.data.results[0][field] = 'testdata';
 });
 
 REQUIRED_CANVAS_CONFIG_FIELDS.forEach((field) => {
-  formData.append(field, 'testdata');
   canvasResponse.data.results[0][field] = 'testdata';
 });
 
 REQUIRED_BLACKBOARD_CONFIG_FIELDS.forEach((field) => {
-  formData.append(field, 'testdata');
   blackboardResponse.data.results[0][field] = 'testdata';
+});
+
+REQUIRED_SUCCESS_FACTOR_CONFIG_FIELDS.forEach((field) => {
+  successFactorsResponse.data.results[0][field] = 'testdata';
 });
 
 const waitForAsync = () => new Promise(resolve => setImmediate(resolve));
 
 describe('<LmsConfigurations /> ', () => {
+  it('get list of forms when all configs return 404s', async () => {
+    LmsApiService.fetchMoodleConfig.mockRejectedValue(notFoundMoodleResponse);
+    LmsApiService.fetchBlackboardConfig.mockRejectedValue(notFoundBlackboardResponse);
+    LmsApiService.fetchCanvasConfig.mockRejectedValue(notFoundCanvasResponse);
+    LmsApiService.fetchSuccessFactorsConfig.mockRejectedValue(notFoundSuccessFactorsResponse);
+
+    const wrapper = mount(<LmsConfigurations enterpriseId="testEnterpriseId" />);
+    await waitForAsync();
+    wrapper.update();
+    expect(wrapper.find('ErrorPage').length).toBe(0);
+    expect(wrapper.find('CollapsibleAdvanced').length).toBeGreaterThan(0);
+  });
   it('get the Moodle LMS configuration when present', async () => {
     LmsApiService.fetchMoodleConfig.mockResolvedValue(moodleResponse);
 
     // Mock the fetch of the other configs to 404, not found
     LmsApiService.fetchBlackboardConfig.mockRejectedValue(notFoundBlackboardResponse);
     LmsApiService.fetchCanvasConfig.mockRejectedValue(notFoundCanvasResponse);
+    LmsApiService.fetchSuccessFactorsConfig.mockRejectedValue(notFoundSuccessFactorsResponse);
 
     const wrapper = mount(<LmsConfigurations enterpriseId="testEnterpriseId" />);
     await waitForAsync();
@@ -75,6 +104,7 @@ describe('<LmsConfigurations /> ', () => {
     // Mock the fetch of the other configs to 404, not found
     LmsApiService.fetchMoodleConfig.mockRejectedValue(notFoundMoodleResponse);
     LmsApiService.fetchCanvasConfig.mockRejectedValue(notFoundCanvasResponse);
+    LmsApiService.fetchSuccessFactorsConfig.mockRejectedValue(notFoundSuccessFactorsResponse);
 
     const wrapper = mount(<LmsConfigurations enterpriseId="testEnterpriseId" />);
     await waitForAsync();
@@ -83,21 +113,51 @@ describe('<LmsConfigurations /> ', () => {
     expect(wrapper.find('BlackboardIntegrationConfigForm').props().config).toEqual(wrapper.state().blackboardConfig);
   });
 
+  it('get the Canvas LMS configuration when present', async () => {
+    LmsApiService.fetchMoodleConfig.mockRejectedValue(notFoundMoodleResponse);
+    LmsApiService.fetchBlackboardConfig.mockRejectedValue(notFoundBlackboardResponse);
+    LmsApiService.fetchCanvasConfig.mockResolvedValue(canvasResponse);
+    LmsApiService.fetchSuccessFactorsConfig.mockRejectedValue(notFoundSuccessFactorsResponse);
+
+    const wrapper = mount(<LmsConfigurations enterpriseId="testEnterpriseId" />);
+    await waitForAsync();
+    wrapper.update();
+    expect(wrapper.state().canvasConfig).toEqual(canvasResponse.data.results[0]);
+    expect(wrapper.find('CanvasIntegrationConfigForm').props().config).toEqual(wrapper.state().canvasConfig);
+  });
+
+  it('get the Success Factors LMS configuration when present', async () => {
+    LmsApiService.fetchMoodleConfig.mockRejectedValue(notFoundMoodleResponse);
+    LmsApiService.fetchBlackboardConfig.mockRejectedValue(notFoundBlackboardResponse);
+    LmsApiService.fetchCanvasConfig.mockRejectedValue(notFoundCanvasResponse);
+    LmsApiService.fetchSuccessFactorsConfig.mockResolvedValue(successFactorsResponse);
+
+    const wrapper = mount(<LmsConfigurations enterpriseId="testEnterpriseId" />);
+    await waitForAsync();
+    wrapper.update();
+    expect(wrapper.state().sapsfConfig).toEqual(successFactorsResponse.data.results[0]);
+    expect(wrapper.find('SuccessFactorsIntegrationConfigForm').props().config).toEqual(wrapper.state().sapsfConfig);
+  });
+
   it('return Error Page when the Moodle LMS fetch request fails with error (not 404)', async () => {
     LmsApiService.fetchMoodleConfig.mockRejectedValue(badMoodleResponse);
 
     // We want to ensure that the Moodle fetch config method will cause the error page to display
     // if it encounters an unexpected error while the other config fetch methods succeed.
     LmsApiService.fetchBlackboardConfig.mockResolvedValue(blackboardResponse);
-    LmsApiService.fetchCanvasConfig.mockResolvedValue(canvasResponse);
+    LmsApiService.fetchCanvasConfig.mockRejectedValue(notFoundCanvasResponse);
+    LmsApiService.fetchSuccessFactorsConfig.mockRejectedValue(notFoundSuccessFactorsResponse);
 
     const wrapper = mount(<LmsConfigurations enterpriseId="testEnterpriseId" />);
     await waitForAsync();
     wrapper.update();
 
-    // Expect the moodle config to be false, while the others to be true
     expect(wrapper.state().moodleConfig).toBeFalsy();
     expect(wrapper.find('ErrorPage').length).toBe(1);
+    expect(wrapper.find('MoodleIntegrationConfigForm').length).toBe(0);
+    expect(wrapper.find('SuccessFactorsIntegrationConfigForm').length).toBe(0);
+    expect(wrapper.find('CanvasIntegrationConfigForm').length).toBe(0);
+    expect(wrapper.find('BlackboardIntegrationConfigForm').length).toBe(0);
   });
 
   it('return Error Page when Blackboard LMS fetch request fails with an error (not 404)', async () => {
@@ -107,25 +167,53 @@ describe('<LmsConfigurations /> ', () => {
     // if it encounters an unexpected error while the other pages succeed.
     LmsApiService.fetchMoodleConfig.mockResolvedValue(blackboardResponse);
     LmsApiService.fetchCanvasConfig.mockResolvedValue(canvasResponse);
+    LmsApiService.fetchSuccessFactorsConfig.mockRejectedValue(notFoundSuccessFactorsResponse);
 
     const wrapper = mount(<LmsConfigurations enterpriseId="testEnterpriseId" />);
     await waitForAsync();
     wrapper.update();
 
-    // Expect the moodle config to be false, while the others to be true
     expect(wrapper.state().blackboardConfig).toBeFalsy();
     expect(wrapper.find('ErrorPage').length).toBe(1);
+    expect(wrapper.find('MoodleIntegrationConfigForm').length).toBe(0);
+    expect(wrapper.find('SuccessFactorsIntegrationConfigForm').length).toBe(0);
+    expect(wrapper.find('CanvasIntegrationConfigForm').length).toBe(0);
+    expect(wrapper.find('BlackboardIntegrationConfigForm').length).toBe(0);
   });
 
-  it('return Fragment/Collapsible components when all have 404', async () => {
+  it('return Error Page when the Canvas LMS fetch request fails with error (not 404)', async () => {
     LmsApiService.fetchMoodleConfig.mockRejectedValue(notFoundMoodleResponse);
-    LmsApiService.fetchCanvasConfig.mockRejectedValue(notFoundCanvasResponse);
-    LmsApiService.fetchBlackboardConfig.mockRejectedValue(notFoundBlackboardResponse);
+    LmsApiService.fetchBlackboardConfig.mockResolvedValue(blackboardResponse);
+    LmsApiService.fetchCanvasConfig.mockRejectedValue(badCanvasResponse);
+    LmsApiService.fetchSuccessFactorsConfig.mockRejectedValue(notFoundSuccessFactorsResponse);
+
     const wrapper = mount(<LmsConfigurations enterpriseId="testEnterpriseId" />);
     await waitForAsync();
     wrapper.update();
-    expect(wrapper.state().moodleConfig).toBeFalsy();
-    expect(wrapper.find('ErrorPage').length).toBe(0);
-    expect(wrapper.find('CollapsibleAdvanced').length).toBe(3);
+
+    expect(wrapper.state().canvasConfig).toBeFalsy();
+    expect(wrapper.find('ErrorPage').length).toBe(1);
+    expect(wrapper.find('MoodleIntegrationConfigForm').length).toBe(0);
+    expect(wrapper.find('SuccessFactorsIntegrationConfigForm').length).toBe(0);
+    expect(wrapper.find('CanvasIntegrationConfigForm').length).toBe(0);
+    expect(wrapper.find('BlackboardIntegrationConfigForm').length).toBe(0);
+  });
+
+  it('return Error Page when the Success Factors LMS fetch request fails with error (not 404)', async () => {
+    LmsApiService.fetchMoodleConfig.mockRejectedValue(notFoundMoodleResponse);
+    LmsApiService.fetchBlackboardConfig.mockResolvedValue(blackboardResponse);
+    LmsApiService.fetchCanvasConfig.mockRejectedValue(notFoundCanvasResponse);
+    LmsApiService.fetchSuccessFactorsConfig.mockRejectedValue(badSuccessFactorsResponse);
+
+    const wrapper = mount(<LmsConfigurations enterpriseId="testEnterpriseId" />);
+    await waitForAsync();
+    wrapper.update();
+
+    expect(wrapper.state().sapsfConfig).toBeFalsy();
+    expect(wrapper.find('ErrorPage').length).toBe(1);
+    expect(wrapper.find('MoodleIntegrationConfigForm').length).toBe(0);
+    expect(wrapper.find('SuccessFactorsIntegrationConfigForm').length).toBe(0);
+    expect(wrapper.find('CanvasIntegrationConfigForm').length).toBe(0);
+    expect(wrapper.find('BlackboardIntegrationConfigForm').length).toBe(0);
   });
 });
