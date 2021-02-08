cd5a6e2ac616f19572fb542dbb45fca8f260739c
edx-abolger <74684094+edx-abolger@users.noreply.github.com>
2021-02-08 15:58:24 -0500

    Fix forward for post requests in axios not needing extra parameter and resulting in form submit failure (#466)

--- src/data/services/ZendeskApiService.js
+++ src/data/services/ZendeskApiService.js
@@ -25,7 +25,7 @@ class ZendeskApiService {
       custom_fields: [],
       tags: ['enterprise_admin_support'],
     };
-    return getAuthenticatedHttpClient().post(url, data, 'json');
+    return getAuthenticatedHttpClient().post(url, data);
   }
 }
 
