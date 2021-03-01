f54a307a836d762c4f5e5ddb91002ccd1733c861
Kellie Selinka <>
2021-03-01 12:35:39 -0500

    update the template activation email displayed on the invite learner modal

--- src/components/InviteLearnersModal/emailTemplate.js
+++ src/components/InviteLearnersModal/emailTemplate.js
@@ -5,9 +5,11 @@ const emailTemplate = {
   body: `{ENTERPRISE_NAME} has partnered with edX to give you an unlimited subscription to learn on edX!  Take the best courses in the most in-demand subject areas and upskill for a new career opportunity.  Earn a professional certificate, start a program or just learn for fun.
 {LICENSE_ACTIVATION_LINK}
 
-So you don’t have to search for this link, bookmark your learning portal now to have easy access to your subscription in the future.
+About edX
 
-{LEARNER_PORTAL_LINK}
+Since 2012, edX has been committed to increasing access to high-quality education for everyone, everywhere. By harnessing the transformative power of education through online learning, edX empowers learners to unlock their potential and become changemakers.
+
+We are excited to welcome you to our growing community of over 35 million users and 15 thousand instructors from 160 partner universities and organizations.
 `,
   closing: getSubscriptionContactText,
 };
