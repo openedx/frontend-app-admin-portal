b4daee03e1cb2966ef699df282afd65c38e56018
binodpant <bpant@edx.org>
2021-03-11 16:15:53 -0500

    feat: replace local history with one from platform

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Router } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
 
 import { AuthenticatedPageRoute, PageRoute, AppProvider } from '@edx/frontend-platform/react';
@@ -16,55 +16,52 @@ import SupportPage from '../SupportPage';
 import { ToastsProvider, Toasts } from '../Toasts';
 
 import store from '../../data/store';
-import history from '../../data/history';
 
 const AppWrapper = () => {
   const apiClient = getAuthenticatedHttpClient();
   return (
     <AppProvider store={store}>
-      <Router history={history}>
-        <ToastsProvider>
-          <Helmet
-            titleTemplate="%s - edX Admin Portal"
-            defaultTitle="edX Admin Portal"
+      <ToastsProvider>
+        <Helmet
+          titleTemplate="%s - edX Admin Portal"
+          defaultTitle="edX Admin Portal"
+        />
+        <Toasts />
+        <Header />
+        <Switch>
+          <PageRoute exact path="/public/support" component={SupportPage} />
+          <AuthenticatedPageRoute
+            path="/enterprises"
+            render={(routerProps) => <EnterpriseIndexPage {...routerProps} />}
+            authenticatedAPIClient={apiClient}
+            redirect={`${process.env.BASE_URL}/enterprises`}
           />
-          <Toasts />
-          <Header />
-          <Switch>
-            <PageRoute exact path="/public/support" component={SupportPage} />
-            <AuthenticatedPageRoute
-              path="/enterprises"
-              render={(routerProps) => <EnterpriseIndexPage {...routerProps} />}
-              authenticatedAPIClient={apiClient}
-              redirect={`${process.env.BASE_URL}/enterprises`}
-            />
-            <PageRoute
-              exact
-              path="/:enterpriseSlug/admin/register"
-              component={AdminRegisterPage}
-            />
-            <PageRoute
-              exact
-              path="/:enterpriseSlug/admin/register/activate"
-              component={UserActivationPage}
-            />
-            <AuthenticatedPageRoute
-              path="/:enterpriseSlug"
-              component={EnterpriseApp}
-              authenticatedAPIClient={apiClient}
-              redirect={process.env.BASE_URL}
-            />
-            <AuthenticatedPageRoute
-              path="/"
-              render={(routerProps) => <EnterpriseIndexPage {...routerProps} />}
-              authenticatedAPIClient={apiClient}
-              redirect={process.env.BASE_URL}
-            />
-            <PageRoute component={NotFoundPage} />
-          </Switch>
-          <Footer />
-        </ToastsProvider>
-      </Router>
+          <PageRoute
+            exact
+            path="/:enterpriseSlug/admin/register"
+            component={AdminRegisterPage}
+          />
+          <PageRoute
+            exact
+            path="/:enterpriseSlug/admin/register/activate"
+            component={UserActivationPage}
+          />
+          <AuthenticatedPageRoute
+            path="/:enterpriseSlug"
+            component={EnterpriseApp}
+            authenticatedAPIClient={apiClient}
+            redirect={process.env.BASE_URL}
+          />
+          <AuthenticatedPageRoute
+            path="/"
+            render={(routerProps) => <EnterpriseIndexPage {...routerProps} />}
+            authenticatedAPIClient={apiClient}
+            redirect={process.env.BASE_URL}
+          />
+          <PageRoute component={NotFoundPage} />
+        </Switch>
+        <Footer />
+      </ToastsProvider>
     </AppProvider>
   );
 };
