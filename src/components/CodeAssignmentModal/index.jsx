f8412dcf1c34f3e7d1ec165f63b0fa9c5d6b8d4e
Lael Birch <lbirch@edx.org>
2021-03-16 18:08:19 -0400

    Get rid of double connect in the CodeAssignmentModal
    Who knows what unintended consequences might occur by connecting a component to redux twice. Moving all redux-related things to the container.

--- src/components/CodeAssignmentModal/index.jsx
+++ src/components/CodeAssignmentModal/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
 import { Field, reduxForm, SubmissionError } from 'redux-form';
 import {
   Button, Input, Icon, Modal,
@@ -24,7 +23,7 @@ import { EMAIL_ADDRESS_TEXT_FORM_DATA, EMAIL_ADDRESS_CSV_FORM_DATA } from '../..
 import './CodeAssignmentModal.scss';
 import { configuration } from '../../config';
 
-class BaseCodeAssignmentModal extends React.Component {
+class CodeAssignmentModal extends React.Component {
   constructor(props) {
     super(props);
 
@@ -554,14 +553,14 @@ class BaseCodeAssignmentModal extends React.Component {
   }
 }
 
-BaseCodeAssignmentModal.defaultProps = {
+CodeAssignmentModal.defaultProps = {
   error: null,
   isBulkAssign: false,
   data: {},
   currentEmail: '',
 };
 
-BaseCodeAssignmentModal.propTypes = {
+CodeAssignmentModal.propTypes = {
   // props from redux
   enterpriseSlug: PropTypes.string.isRequired,
   enterpriseUuid: PropTypes.string.isRequired,
@@ -600,13 +599,6 @@ BaseCodeAssignmentModal.propTypes = {
     remainingUses: PropTypes.number,
   }),
 };
-
-const mapStateToProps = state => ({
-  enterpriseSlug: state.portalConfiguration.enterpriseSlug,
-  enableLearnerPortal: state.portalConfiguration.enableLearnerPortal,
-});
-
-const CodeAssignmentModal = connect(mapStateToProps)(BaseCodeAssignmentModal);
 
 export default reduxForm({
   form: 'code-assignment-modal-form',
