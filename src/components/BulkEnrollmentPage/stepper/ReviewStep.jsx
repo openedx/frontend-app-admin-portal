49dff7bc1f2a2f8e18af253c8ed8b376b80d7556
Lael Birch <lbirch@edx.org>
2021-04-23 13:53:13 -0400

    Add tests for BulkEnrollment stepper

--- src/components/BulkEnrollmentPage/stepper/ReviewStep.jsx
+++ src/components/BulkEnrollmentPage/stepper/ReviewStep.jsx
@@ -2,13 +2,14 @@ import React, { useContext } from 'react';
 // import PropTypes from 'prop-types';
 
 import { BulkEnrollContext } from '../BulkEnrollmentContext';
+import { REVIEW_TITLE } from './constants';
 
 const ReviewStep = () => {
   const { emails: [selectedEmails], courses: [selectedCourses] } = useContext(BulkEnrollContext);
 
   return (
     <>
-      <h2>Review</h2>
+      <h2>{REVIEW_TITLE}</h2>
       <h3>Emails</h3>
       <ul>
         {selectedEmails.map((email) => <li>{email}</li>)}
