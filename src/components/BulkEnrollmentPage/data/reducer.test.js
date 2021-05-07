80d1e0f26c640292d56631ff05ad06b4211859e4
Lael Birch <lbirch@edx.org>
2021-05-07 10:04:34 -0400

    Reducer & action tests

--- src/components/BulkEnrollmentPage/data/reducer.test.js
+++ src/components/BulkEnrollmentPage/data/reducer.test.js
@@ -1,5 +1,7 @@
 import selectedRowsReducer from './reducer';
-import { setSelectedRowsAction, deleteSelectedRowAction } from './actions';
+import {
+  setSelectedRowsAction, deleteSelectedRowAction, clearSelectionAction, addSelectedRowAction,
+} from './actions';
 
 describe('selectedRowsReducer', () => {
   it('can set rows', () => {
@@ -12,5 +14,26 @@ describe('selectedRowsReducer', () => {
     const expected = [{ id: 3 }];
     const result = selectedRowsReducer(selectedRows, deleteSelectedRowAction(2));
     expect(result).toEqual(expected);
+  });
+  test.each(
+    [
+      [[{ id: 'foo' }]],
+      [[{ id: 'foo' }, { id: 'bar' }]],
+      [[]],
+    ],
+  )('can clear all rows %#', (selectedRows) => {
+    const result = selectedRowsReducer(selectedRows, clearSelectionAction());
+    expect(result).toEqual([]);
+  });
+  test.each(
+    [
+      [[{ id: 'foo' }]],
+      [[{ id: 'foo' }, { id: 'bar' }]],
+      [[]],
+    ],
+  )('can add one row %#', (selectedRows) => {
+    const newRow = { id: '1235' };
+    const result = selectedRowsReducer(selectedRows, addSelectedRowAction(newRow));
+    expect(result).toEqual([...selectedRows, newRow]);
   });
 });
