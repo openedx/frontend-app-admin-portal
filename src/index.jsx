ed9948db9e380ba75b925c72677f4696bc79c81a
Adam Stankiewicz <agstanki@gmail.com>
2021-06-03 12:16:40 -0400

    build: upgrade frontend-platform (#569)

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,7 @@ import 'regenerator-runtime/runtime';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {
-  initialize, APP_INIT_ERROR, APP_READY, subscribe,
+  initialize, APP_INIT_ERROR, APP_READY, subscribe, mergeConfig,
 } from '@edx/frontend-platform';
 import { ErrorPage } from '@edx/frontend-platform/react';
 
@@ -25,6 +25,15 @@ subscribe(APP_INIT_ERROR, (error) => {
 });
 
 initialize({
+  handlers: {
+    config: () => {
+      mergeConfig({
+        // Logs JS errors matching the following regex as NewRelic page actions instead of
+        // errors,reducing JS error noise.
+        IGNORED_ERROR_REGEX: '(Axios Error|\'removeChild\'|Script error|getReadModeExtract)',
+      });
+    },
+  },
   messages: [],
   requireAuthenticatedUser: false,
   hydrateAuthenticatedUser: true,
