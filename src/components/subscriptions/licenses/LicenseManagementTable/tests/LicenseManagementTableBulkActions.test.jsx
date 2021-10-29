88dd04e4b3a64839ee271d671a6260408898b087
manny-m <mmoquete@edx.org>
2021-10-29 15:13:02 -0400

    fix: bulkaction modals

--- src/components/subscriptions/licenses/LicenseManagementTable/tests/LicenseManagementTableBulkActions.test.jsx
+++ src/components/subscriptions/licenses/LicenseManagementTable/tests/LicenseManagementTableBulkActions.test.jsx
@@ -6,6 +6,9 @@ import {
   cleanup,
 } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import moment from 'moment';
+import configureMockStore from 'redux-mock-store';
+import { Provider } from 'react-redux';
 
 import LicenseManagementTableBulkActions from '../LicenseManagementTableBulkActions';
 import {
@@ -14,19 +17,41 @@ import {
   REVOKED,
 } from '../../../data/constants';
 
+const mockStore = configureMockStore();
+const store = mockStore({
+  portalConfiguration: {
+    enterpriseId: 'test-enterprise-id',
+  },
+});
+
 const basicProps = {
   selectedUsers: [],
-  bulkRemindOnClick: () => {},
-  bulkRevokeOnClick: () => {},
+  onRemindSuccess: () => {},
+  onRevokeSuccess: () => {},
   allUsersSelected: false,
   activatedUsers: 0,
   assignedUsers: 0,
+  subscription: {
+    uuid: '1',
+    expirationDate: moment().add(1, 'days').format(),
+    isRevocationCapEnabled: false,
+    revocations: {
+      applied: 0,
+      remaining: 10,
+    },
+  },
 };
 
 const testAssignedUser = { status: ASSIGNED };
 const testActivatedUser = { status: ACTIVATED };
 const testRevokedUser = { status: REVOKED };
 const testUndefinedUser = { status: 'foo' };
+
+const LicenseManagementTableBulkActionsWithContext = (props) => (
+  <Provider store={store}>
+    <LicenseManagementTableBulkActions {...props} />
+  </Provider>
+);
 
 describe('<LicenseManagementTableBulkActions />', () => {
   afterEach(() => {
@@ -35,7 +60,7 @@ describe('<LicenseManagementTableBulkActions />', () => {
   });
 
   it('renders correct empty state', () => {
-    render(<LicenseManagementTableBulkActions {...basicProps} />);
+    render(<LicenseManagementTableBulkActionsWithContext {...basicProps} />);
     expect(screen.getAllByRole('button').length).toBe(3);
     expect(screen.getByText('Enroll')).toBeTruthy();
     expect(screen.getByText('Remind (0)')).toBeTruthy();
@@ -45,7 +70,7 @@ describe('<LicenseManagementTableBulkActions />', () => {
   describe('renders correct label when not all users are selected ', () => {
     it('selected only revoked users', () => {
       const props = { ...basicProps, selectedUsers: [testRevokedUser, testRevokedUser] };
-      render(<LicenseManagementTableBulkActions {...props} />);
+      render(<LicenseManagementTableBulkActionsWithContext {...props} />);
       expect(screen.getByText('Enroll')).toBeTruthy();
       expect(screen.getByText('Remind (0)')).toBeTruthy();
       const revokeMenu = screen.getByTestId('revokeToggle');
@@ -56,7 +81,7 @@ describe('<LicenseManagementTableBulkActions />', () => {
     });
     it('selected only activated users', () => {
       const props = { ...basicProps, selectedUsers: [testActivatedUser] };
-      render(<LicenseManagementTableBulkActions {...props} />);
+      render(<LicenseManagementTableBulkActionsWithContext {...props} />);
       expect(screen.getByText('Enroll')).toBeTruthy();
       expect(screen.getByText('Remind (0)')).toBeTruthy();
       const revokeMenu = screen.getByTestId('revokeToggle');
@@ -67,7 +92,7 @@ describe('<LicenseManagementTableBulkActions />', () => {
     });
     it('selected only assigned users', () => {
       const props = { ...basicProps, selectedUsers: [testAssignedUser, testAssignedUser] };
-      render(<LicenseManagementTableBulkActions {...props} />);
+      render(<LicenseManagementTableBulkActionsWithContext {...props} />);
       expect(screen.getByText('Enroll')).toBeTruthy();
       expect(screen.getByText('Remind (2)')).toBeTruthy();
       const revokeMenu = screen.getByTestId('revokeToggle');
@@ -78,7 +103,7 @@ describe('<LicenseManagementTableBulkActions />', () => {
     });
     it('selected mix users', () => {
       const props = { ...basicProps, selectedUsers: [testRevokedUser, testActivatedUser, testAssignedUser] };
-      render(<LicenseManagementTableBulkActions {...props} />);
+      render(<LicenseManagementTableBulkActionsWithContext {...props} />);
       expect(screen.getByText('Enroll')).toBeTruthy();
       expect(screen.getByText('Remind (1)')).toBeTruthy();
       const revokeMenu = screen.getByTestId('revokeToggle');
@@ -89,7 +114,7 @@ describe('<LicenseManagementTableBulkActions />', () => {
     });
     it('selected undefined users', () => {
       const props = { ...basicProps, selectedUsers: [testUndefinedUser] };
-      render(<LicenseManagementTableBulkActions {...props} />);
+      render(<LicenseManagementTableBulkActionsWithContext {...props} />);
       expect(screen.getByText('Enroll')).toBeTruthy();
       expect(screen.getByText('Remind (0)')).toBeTruthy();
       const revokeMenu = screen.getByTestId('revokeToggle');
@@ -97,6 +122,7 @@ describe('<LicenseManagementTableBulkActions />', () => {
       expect(screen.getByText('Revoke (0)')).toBeTruthy();
     });
   });
+
   it('renders correct label when all users are selected', () => {
     const props = {
       ...basicProps,
@@ -104,7 +130,7 @@ describe('<LicenseManagementTableBulkActions />', () => {
       activatedUsers: 1,
       assignedUsers: 1,
     };
-    render(<LicenseManagementTableBulkActions {...props} />);
+    render(<LicenseManagementTableBulkActionsWithContext {...props} />);
     expect(screen.getByText('Enroll')).toBeTruthy();
     expect(screen.getByText('Remind (1)')).toBeTruthy();
     const revokeMenu = screen.getByTestId('revokeToggle');
@@ -112,5 +138,44 @@ describe('<LicenseManagementTableBulkActions />', () => {
       userEvent.click(revokeMenu);
     });
     expect(screen.getByText('Revoke (2)')).toBeTruthy();
+  });
+
+  it('opens and closes remind modal', () => {
+    const props = { ...basicProps, selectedUsers: [testAssignedUser] };
+    render(<LicenseManagementTableBulkActionsWithContext {...props} />);
+    // open dialog
+    const remindButton = screen.getByText('Remind (1)');
+    act(() => {
+      userEvent.click(remindButton);
+    });
+    expect(screen.getByRole('dialog')).toBeTruthy();
+    // close dialog
+    const cancelButton = screen.getByText('Cancel');
+    act(() => {
+      userEvent.click(cancelButton);
+    });
+    expect(screen.queryByRole('dialog')).toBeFalsy();
+  });
+
+  it('opens and closes revoke modal', () => {
+    const props = { ...basicProps, selectedUsers: [testAssignedUser] };
+    render(<LicenseManagementTableBulkActionsWithContext {...props} />);
+    // reveal revoke menu
+    const revokeMenu = screen.getByTestId('revokeToggle');
+    act(() => {
+      userEvent.click(revokeMenu);
+    });
+    // open dialog
+    const revokeButton = screen.getByText('Revoke (1)');
+    act(() => {
+      userEvent.click(revokeButton);
+    });
+    expect(screen.getByRole('dialog')).toBeTruthy();
+    // close dialog
+    const cancelButton = screen.getByText('Cancel');
+    act(() => {
+      userEvent.click(cancelButton);
+    });
+    expect(screen.queryByRole('dialog')).toBeFalsy();
   });
 });
