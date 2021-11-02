6ba609572ef50411ab86b356b34a800dfbc932a1
Manny <manny-m@users.noreply.github.com>
2021-11-02 11:07:52 -0400

    feat: table actions clear selection (#640)

--- src/components/subscriptions/licenses/LicenseManagementModals/LicenseManagementModalHook.js
+++ src/components/subscriptions/licenses/LicenseManagementModals/LicenseManagementModalHook.js
@@ -16,4 +16,25 @@ export const useRequestState = (isOpen) => {
   return [requestState, setRequestState, initialRequestState];
 };
 
+export const licenseManagementModalZeroState = {
+  isOpen: false,
+  users: [],
+  allUsersSelected: false,
+};
+
+/**
+ * Handles basic modal open/close usage and can hold user data
+ * @param initialState shape:
+ * {
+ *  isOpen: bool
+ *  users: array
+ *  allUsersSelected: bool
+ * }
+ * @returns [state, setState, zeroState]
+ */
+export const useLicenseManagementModalState = (initialState = licenseManagementModalZeroState) => {
+  const [modalState, setModalState] = useState(initialState);
+  return [modalState, setModalState, licenseManagementModalZeroState];
+};
+
 export default useRequestState;
--- src/components/subscriptions/licenses/LicenseManagementTable/LicenseManagementTableBulkActions.jsx
+++ src/components/subscriptions/licenses/LicenseManagementTable/LicenseManagementTableBulkActions.jsx
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import {
   ActionRow, Button, Icon, ModalPopup, useToggle,
 } from '@edx/paragon';
-
 import {
   BookOpen,
   Email,
@@ -12,19 +11,30 @@ import {
 } from '@edx/paragon/icons';
 
 import { canRemindLicense, canRevokeLicense } from '../../data/utils';
+import LicenseManagementRevokeModal from '../LicenseManagementModals/LicenseManagementRevokeModal';
+import LicenseManagementRemindModal from '../LicenseManagementModals/LicenseManagementRemindModal';
+import {
+  useLicenseManagementModalState,
+  licenseManagementModalZeroState as modalZeroState,
+} from '../LicenseManagementModals/LicenseManagementModalHook';
 
 const LicenseManagementTableBulkActions = ({
+  subscription,
   enrollmentLink,
   selectedUsers,
-  bulkRemindOnClick,
-  bulkRevokeOnClick,
+  onRemindSuccess,
+  onRevokeSuccess,
   allUsersSelected,
   activatedUsers,
   assignedUsers,
   disabled,
 }) => {
+  const [revokeModal, setRevokeModal] = useLicenseManagementModalState();
+  const [remindModal, setRemindModal] = useLicenseManagementModalState();
+
   const [isOpen, open, close] = useToggle(false);
   const target = React.useRef(null);
+
   // Divides selectedUsers users into two arrays
   const [usersToRemind, usersToRevoke] = useMemo(() => {
     if (allUsersSelected) {
@@ -46,44 +56,90 @@ const LicenseManagementTableBulkActions = ({
     return [tempRemind, tempRevoke];
   }, [selectedUsers, allUsersSelected]);
 
+  const revokeOnClick = (revokeUsers) => {
+    setRevokeModal({
+      isOpen: true,
+      users: revokeUsers,
+      allUsersSelected,
+    });
+  };
+
+  const remindOnClick = (remindUsers) => {
+    setRemindModal({
+      isOpen: true,
+      users: remindUsers,
+      allUsersSelected,
+    });
+  };
+
+  const handleRevokeSuccess = () => {
+    setRevokeModal(modalZeroState);
+    onRevokeSuccess();
+  };
+
+  const handleRemindSuccess = () => {
+    setRemindModal(modalZeroState);
+    onRemindSuccess();
+  };
+
   return (
-    <ActionRow>
-      <Button
-        ref={target}
-        variant="tertiary"
-        onClick={open}
-        data-testid="revokeToggle"
-      >
-        <Icon src={MoreVert} />
-      </Button>
-      <ModalPopup positionRef={target} isOpen={isOpen} onClose={close}>
-        <div className="bg-white p-3 rounded shadow">
-          <Button
-            variant="outline-danger"
-            iconBefore={RemoveCircle}
-            onClick={() => bulkRevokeOnClick(usersToRevoke, allUsersSelected)}
-            disabled={(!usersToRevoke.length && !allUsersSelected) || disabled}
-          >
-            Revoke ({allUsersSelected ? activatedUsers + assignedUsers : usersToRevoke.length})
-          </Button>
-        </div>
-      </ModalPopup>
-      <Button
-        variant="outline-primary"
-        iconBefore={Email}
-        onClick={() => bulkRemindOnClick(usersToRemind, allUsersSelected)}
-        disabled={(!usersToRemind.length && !allUsersSelected) || disabled}
-      >
-        Remind ({allUsersSelected ? assignedUsers : usersToRemind.length })
-      </Button>
-      <Button
-        variant="primary"
-        href={enrollmentLink}
-        iconBefore={BookOpen}
-      >
-        Enroll
-      </Button>
-    </ActionRow>
+    <>
+      <ActionRow>
+        <Button
+          ref={target}
+          variant="tertiary"
+          onClick={open}
+          data-testid="revokeToggle"
+        >
+          <Icon src={MoreVert} />
+        </Button>
+        <ModalPopup positionRef={target} isOpen={isOpen} onClose={close}>
+          <div>
+            <Button
+              variant="outline-danger"
+              iconBefore={RemoveCircle}
+              onClick={() => revokeOnClick(usersToRevoke, allUsersSelected)}
+              disabled={(!usersToRevoke.length && !allUsersSelected) || disabled}
+            >
+              Revoke ({allUsersSelected ? activatedUsers + assignedUsers : usersToRevoke.length})
+            </Button>
+          </div>
+        </ModalPopup>
+        <Button
+          variant="outline-primary"
+          iconBefore={Email}
+          onClick={() => remindOnClick(usersToRemind)}
+          disabled={(!usersToRemind.length && !allUsersSelected) || disabled}
+        >
+          Remind ({allUsersSelected ? assignedUsers : usersToRemind.length })
+        </Button>
+        <Button
+          variant="primary"
+          href={enrollmentLink}
+          iconBefore={BookOpen}
+        >
+          Enroll
+        </Button>
+      </ActionRow>
+      <LicenseManagementRevokeModal
+        isOpen={revokeModal.isOpen}
+        usersToRevoke={revokeModal.users}
+        subscription={subscription}
+        onClose={() => setRevokeModal(modalZeroState)}
+        onSuccess={handleRevokeSuccess}
+        revokeAllUsers={revokeModal.allUsersSelected}
+        totalToRevoke={activatedUsers + assignedUsers}
+      />
+      <LicenseManagementRemindModal
+        isOpen={remindModal.isOpen}
+        usersToRemind={remindModal.users}
+        subscription={subscription}
+        onClose={() => setRemindModal(modalZeroState)}
+        onSuccess={handleRemindSuccess}
+        remindAllUsers={remindModal.allUsersSelected}
+        totalToRemind={assignedUsers}
+      />
+    </>
   );
 };
 
@@ -92,14 +148,23 @@ LicenseManagementTableBulkActions.defaultProps = {
 };
 
 LicenseManagementTableBulkActions.propTypes = {
+  subscription: PropTypes.shape({
+    uuid: PropTypes.string.isRequired,
+    expirationDate: PropTypes.string.isRequired,
+    isRevocationCapEnabled: PropTypes.bool.isRequired,
+    revocations: PropTypes.shape({
+      applied: PropTypes.number.isRequired,
+      remaining: PropTypes.number.isRequired,
+    }),
+  }).isRequired,
   enrollmentLink: PropTypes.string.isRequired,
   selectedUsers: PropTypes.arrayOf(
     PropTypes.shape({
       status: PropTypes.string.isRequired,
     }).isRequired,
   ).isRequired,
-  bulkRemindOnClick: PropTypes.func.isRequired,
-  bulkRevokeOnClick: PropTypes.func.isRequired,
+  onRemindSuccess: PropTypes.func.isRequired,
+  onRevokeSuccess: PropTypes.func.isRequired,
   allUsersSelected: PropTypes.bool.isRequired,
   activatedUsers: PropTypes.number.isRequired,
   assignedUsers: PropTypes.number.isRequired,
