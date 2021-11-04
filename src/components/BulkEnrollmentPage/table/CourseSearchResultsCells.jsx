82e29db3036135a7a5dd8744d2dba2a70ecfdb67
Kira <kmiller@edx.org>
2021-11-04 14:22:45 -0400

    feat: add popover to course search title

--- src/components/BulkEnrollmentPage/table/CourseSearchResultsCells.jsx
+++ src/components/BulkEnrollmentPage/table/CourseSearchResultsCells.jsx
@@ -1,18 +1,42 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Popover, Button, OverlayTrigger } from '@edx/paragon';
 
 import moment from 'moment';
 
 import { configuration } from '../../../config';
 
+const HREF_TITLE = 'Learn more about this course';
+
 export const CourseNameCell = ({ value, row, enterpriseSlug }) => (
-  <a
-    href={`${configuration.ENTERPRISE_LEARNER_PORTAL_URL}/${enterpriseSlug}/course/${row?.original?.key}`}
-    target="_blank"
-    rel="noopener noreferrer"
+  <OverlayTrigger
+    trigger="click"
+    rootClose
+    key="top"
+    placement="top"
+    overlay={(
+      <Popover id="popover-positioned-top">
+        <Popover.Title>{value}</Popover.Title>
+        <Popover.Content>
+          <div
+            className="desc"
+            /* eslint-disable-next-line */
+            dangerouslySetInnerHTML={{ __html: row?.original?.short_description }}
+          />
+          <hr />
+          <a
+            href={`${configuration.ENTERPRISE_LEARNER_PORTAL_URL}/${enterpriseSlug}/course/${row?.original?.key}`}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {HREF_TITLE}
+          </a>
+        </Popover.Content>
+      </Popover>
+      )}
   >
-    {value}
-  </a>
+    <Button variant="link">{value}</Button>
+  </OverlayTrigger>
 );
 
 CourseNameCell.propTypes = {
