472fc08ba2944414ece212ebe1bcc52fd6080967
binodpant <bpant@edx.org>
2021-11-10 21:10:26 -0500

    test: test fix for addCoursesStep

--- src/components/BulkEnrollmentPage/stepper/AddCoursesStep.test.jsx
+++ src/components/BulkEnrollmentPage/stepper/AddCoursesStep.test.jsx
@@ -3,7 +3,7 @@ import '@testing-library/jest-dom/extend-expect';
 import React from 'react';
 
 import { ADD_COURSES_TITLE, WARNING_ALERT_TITLE_TEXT } from './constants';
-import BulkEnrollContextProvider from '../BulkEnrollmentContext';
+import { BulkEnrollContext } from '../BulkEnrollmentContext';
 import { TABLE_HEADERS } from '../CourseSearchResults';
 
 import '../../../../__mocks__/react-instantsearch-dom';
@@ -15,14 +15,22 @@ const defaultProps = {
   enterpriseSlug: 'sluggy',
   subscription: { uuid: 'foo', enterpriseCatalogUuid: 'bar' },
   enterpriseId: 'fancyEnt',
-  selectedCoursesNum: 8,
 };
 
-const StepperWrapper = (props) => (
-  <BulkEnrollContextProvider>
-    <AddCoursesStep {...props} />
-  </BulkEnrollContextProvider>
-);
+const StepperWrapper = (props) => {
+  const selectedCourses = [...Array(8).keys()].map(n => `course-${n}`);
+  const selectedEmails = [];
+  const value = {
+    courses: [selectedCourses, () => {}],
+    emails: [selectedEmails, () => {}],
+    subscription: [{}, () => {}],
+  };
+  return (
+    <BulkEnrollContext.Provider value={value}>
+      <AddCoursesStep {...props} />
+    </BulkEnrollContext.Provider>
+  );
+};
 
 describe('AddCoursesStep', () => {
   it('displays a title', () => {
@@ -34,7 +42,7 @@ describe('AddCoursesStep', () => {
     expect(screen.getByText(TABLE_HEADERS.courseName)).toBeInTheDocument();
     expect(screen.getByText(TABLE_HEADERS.courseAvailability)).toBeInTheDocument();
   });
-  it('displays that warning dialog text', () => {
+  it('more than max selected courses causes display of warning dialog text', () => {
     renderWithRouter(<StepperWrapper {...defaultProps} />);
     expect(screen.getByText(WARNING_ALERT_TITLE_TEXT)).toBeInTheDocument();
   });
