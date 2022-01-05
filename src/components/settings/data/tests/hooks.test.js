944caee5d40d1911d98e758fa474f77c0b857fc2
manny-m <mmoquete@edx.org>
2022-01-05 09:47:40 -0500

    fix: hook tests

--- src/components/settings/data/tests/hooks.test.js
+++ src/components/settings/data/tests/hooks.test.js
@@ -3,13 +3,16 @@ import { renderHook, act } from '@testing-library/react-hooks/dom';
 import { waitFor } from '@testing-library/react';
 
 import LmsApiService from '../../../../data/services/LmsApiService';
+import LicenseManagerApiService from '../../../../data/services/LicenseManagerAPIService';
 import {
   useCurrentSettingsTab,
   useLinkManagement,
+  useCustomerAgreementData,
 } from '../hooks';
 
 const TEST_ENTERPRISE_UUID = 'test-enterprise-uuid';
 jest.mock('../../../../data/services/LmsApiService');
+jest.mock('../../../../data/services/LicenseManagerApiService');
 
 jest.mock('react-router-dom', () => ({
   ...jest.requireActual('react-router-dom'),
@@ -60,6 +63,43 @@ describe('settings hooks', () => {
           links: expectedData,
           loadingLinks: false,
           refreshLinks: expect.any(Function),
+        });
+      });
+    });
+  });
+
+  describe('useCustomerAgreementData', () => {
+    beforeEach(() => {
+      jest.resetAllMocks();
+    });
+    test('success', () => {
+      const NET_DAYS_UNTIL_EXPIRATION = 100;
+      const expectedResponse = {
+        data: {
+          count: 1,
+          results: [{
+            enterprise_customer_slug: 'test-enterprise',
+            net_days_until_expiration: NET_DAYS_UNTIL_EXPIRATION,
+            ordered_subscription_plan_expirations: [{ title: 'Test Sub' }],
+            subscription_for_auto_applied_licenses: null,
+            subscriptions: [{ title: 'Test Sub' }],
+            uuid: '54358914-ca53-469e-a478-c591b7ec346e',
+          }],
+        },
+      };
+      LicenseManagerApiService.fetchCustomerAgreementData.mockResolvedValue(expectedResponse);
+      let result;
+      act(() => {
+        ({ result } = renderHook(() => useCustomerAgreementData(TEST_ENTERPRISE_UUID)));
+        expect(result.current).toStrictEqual({
+          customerAgreement: { netDaysUntilExpiration: 0 },
+          loadingCustomerAgreement: true,
+        });
+      });
+      waitFor(() => {
+        expect(result.current).toStrictEqual({
+          customerAgreement: { netDaysUntilExpiration: NET_DAYS_UNTIL_EXPIRATION },
+          loadingLinks: false,
         });
       });
     });
