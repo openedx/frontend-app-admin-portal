a61c321b6b473607ca188a058d116a206f8b3bea
Long Lin <lin.longsheng01@gmail.com>
2022-03-02 14:18:31 -0500

    commit dis

---
+++ src/containers/CodeManagementPage/data/constants.js
@@ -0,0 +1,7 @@
+export const MANAGE_CODES_TAB = 'coupons';
+export const MANAGE_REQUESTS_TAB = 'enrollment-requests';
+
+export const MANAGE_CODES_LABEL = 'Manage Codes';
+export const MANAGE_REQUESTS_LABEL = 'Manage Requests';
+
+export const DEFAULT_TAB = MANAGE_CODES_TAB;
