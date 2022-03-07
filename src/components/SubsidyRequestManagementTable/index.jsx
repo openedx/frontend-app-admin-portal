4a4cddf89ff281c0c0e5cc88c6b04504ec5af362
Adam Stankiewicz <agstanki@gmail.com>
2022-03-07 07:19:41 -0500

    fix: add debounce and support filter by email

--- src/components/SubsidyRequestManagementTable/index.jsx
+++ src/components/SubsidyRequestManagementTable/index.jsx
@@ -13,11 +13,25 @@ import CourseNameCell from './CourseNameCell';
 import ActionCell from './ActionCell';
 import { capitalizeFirstLetter } from '../../utils';
 
-export const transformRequestOverview = requestStates => requestStates.map(({ state, count }) => ({
-  name: capitalizeFirstLetter(state),
-  number: count,
-  value: state,
-}));
+export const transformRequestOverview = (requestStates) => {
+  const requestOverview = {
+    requested: 0,
+    approved: 0,
+    declined: 0,
+  };
+  // There are other request states beyond "requested", "approved", and "declined" includin
+  // "pending" and "error". By default, we do not want to display these in the request status
+  // filters as they are implementation details that should only be exposed when request states
+  // are actually in that state.
+  requestStates.forEach(({ state, count }) => {
+    requestOverview[state] = count;
+  });
+  return Object.entries(requestOverview).map(([state, count]) => ({
+    name: capitalizeFirstLetter(state),
+    number: count,
+    value: state,
+  }));
+};
 
 export const transformRequests = requests => requests.map((request) => ({
   uuid: request.uuid,
