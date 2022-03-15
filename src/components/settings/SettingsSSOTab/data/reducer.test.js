900f45d8c96dd63ddb88cd20fe08ffbd547ff1f3
binodpant <bpant@edx.org>
2022-03-15 16:58:37 -0400

    test: Reducer tests

---
+++ src/components/settings/SettingsSSOTab/data/reducer.test.js
@@ -0,0 +1,56 @@
+import {
+  updateCurrentstep,
+  updateIdpEntryTypeAction,
+  updateIdpMetadataURLAction,
+  updateServiceProviderConfigured,
+} from './actions';
+import SSOStateReducer from './reducer';
+
+describe('reducer sso tests', () => {
+  test('adds metadataURL', () => {
+    expect(SSOStateReducer({}, updateIdpMetadataURLAction('aTestURL'))).toStrictEqual({
+      idp: { metadataURL: 'aTestURL' },
+    });
+  });
+
+  test('overwrites metadataURL', () => {
+    expect(SSOStateReducer({
+      idp: {
+        metadataURL: 'oldOne',
+      },
+    }, updateIdpMetadataURLAction('aTestURL'))).toStrictEqual({
+      idp: { metadataURL: 'aTestURL' },
+    });
+  });
+  test('overwrites metadataURL', () => {
+    expect(SSOStateReducer({
+      idp: {
+        metadataURL: 'a',
+        entryType: 'oldOne',
+      },
+    }, updateIdpEntryTypeAction('typeII'))).toStrictEqual({
+      idp: { metadataURL: 'a', entryType: 'typeII' },
+    });
+  });
+  test('overwrites currentStep', () => {
+    expect(SSOStateReducer({
+      currentStep: 'abc',
+      idp: {
+        metadataURL: 'a',
+        entryType: 'oldOne',
+      },
+    }, updateCurrentstep('aGiantLeapForMankind'))).toStrictEqual({
+      idp: { metadataURL: 'a', entryType: 'oldOne' },
+      currentStep: 'aGiantLeapForMankind',
+    });
+  });
+  test('overwrites isSPConfigured', () => {
+    expect(SSOStateReducer({
+      serviceprovider: { isSPConfigured: false },
+      currentStep: 'abc',
+    }, updateServiceProviderConfigured(true))).toStrictEqual({
+      serviceprovider: { isSPConfigured: true },
+      currentStep: 'abc',
+    });
+  });
+});
