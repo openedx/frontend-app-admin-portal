a9be9bb83064be9a724bce47c18782b16e5029df
binodpant <bpant@edx.org>
2022-03-29 15:29:31 -0400

    test: test for utils

---
+++ src/components/settings/SettingsSSOTab/tests/utils.test.js
@@ -0,0 +1,14 @@
+import LmsApiService from '../../../../data/services/LmsApiService';
+import { updateSamlProviderData } from '../utils';
+
+jest.mock('../../../data/services/LmsApiService', () => ({
+  syncProviderData: jest.fn(),
+}));
+describe('update saml provider data', async () => {
+  const enterpriseId = 'a-id';
+  const metadataURL = 'http://url';
+  const entityID = 'anId';
+  LmsApiService.syncProviderData.mockResolvedValue({ lepard: 'def' });
+  const value = await updateSamlProviderData({ enterpriseId, metadataURL, entityID });
+  expect(value).toBe({ lepard: 'def' });
+});
