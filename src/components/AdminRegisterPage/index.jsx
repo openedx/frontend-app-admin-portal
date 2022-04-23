0b940af38c71e2aacc998c58991ed9cd7525dff5
irfanuddinahmad <irfan.ahmad@arbisoft.com>
2022-04-23 11:11:11 +0500

    fix: upgraded frontend-enterprise-utils to latest version

--- src/components/AdminRegisterPage/index.jsx
+++ src/components/AdminRegisterPage/index.jsx
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { Redirect, withRouter } from 'react-router-dom';
+import { useHistory, withRouter } from 'react-router-dom';
+import { logError } from '@edx/frontend-platform/logging';
 import { getAuthenticatedUser, getLogoutRedirectUrl } from '@edx/frontend-platform/auth';
 import { LoginRedirect, getProxyLoginUrl } from '@edx/frontend-enterprise-logistration';
 import { isEnterpriseUser, ENTERPRISE_ADMIN } from '@edx/frontend-enterprise-utils';
 
 import EnterpriseAppSkeleton from '../EnterpriseApp/EnterpriseAppSkeleton';
+import LmsApiService from '../../data/services/LmsApiService';
 
 const AdminRegisterPage = ({ match }) => {
   const user = getAuthenticatedUser();
+  const { enterpriseSlug } = match.params;
+  const history = useHistory();
 
   if (!user) {
     // user is not authenticated, so redirect to enterprise proxy login flow
@@ -19,18 +23,33 @@ const AdminRegisterPage = ({ match }) => {
     );
   }
 
-  const { enterpriseSlug } = match.params;
-  const isEnterpriseAdmin = isEnterpriseUser(user, ENTERPRISE_ADMIN);
-  if (isEnterpriseAdmin) {
-    // user is authenticated and has the ``enterprise_admin`` JWT role, so redirect user to
-    // account activation page to ensure they verify their email address.
-    return <Redirect to={`/${enterpriseSlug}/admin/register/activate`} />;
-  }
+  useEffect(() => {
+    const processEnterpriseAdmin = (enterpriseUUID) => {
+      const isEnterpriseAdmin = isEnterpriseUser(user, ENTERPRISE_ADMIN, enterpriseUUID);
+      if (isEnterpriseAdmin) {
+        // user is authenticated and has the ``enterprise_admin`` JWT role, so redirect user to
+        // account activation page to ensure they verify their email address.
+        history.push(`/${enterpriseSlug}/admin/register/activate`);
+      } else {
+        // user is authenticated but doesn't have the `enterprise_admin` JWT role; force a log out so their
+        // JWT roles gets refreshed. on their next login, the JWT roles will be updated.
+        const logoutRedirectUrl = getLogoutRedirectUrl(getProxyLoginUrl(enterpriseSlug));
+        global.location.href = logoutRedirectUrl;
+      }
+      return <EnterpriseAppSkeleton />;
+    };
 
-  // user is authenticated but doesn't have the `enterprise_admin` JWT role; force a log out so their
-  // JWT roles gets refreshed. on their next login, the JWT roles will be updated.
-  const logoutRedirectUrl = getLogoutRedirectUrl(getProxyLoginUrl(enterpriseSlug));
-  global.location.href = logoutRedirectUrl;
+    LmsApiService.fetchEnterpriseBySlug(enterpriseSlug)
+      .then((response) => {
+        if (response.data && response.data.uuid) {
+          processEnterpriseAdmin(response.data.uuid);
+        }
+      })
+      .catch((error) => {
+        logError(error);
+      });
+  }, [user, enterpriseSlug]);
+
   return <EnterpriseAppSkeleton />;
 };
 
