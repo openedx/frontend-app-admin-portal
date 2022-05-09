822394753b048148733c97ccc69d73059ca872c0
Alexander Sheehan <asheehan@edx.org>
2022-05-09 11:45:54 -0400

    fix: mocking LMS api call hooks in configure step tests

--- src/components/settings/SettingsSSOTab/steps/SSOConfigConnectStep.test.jsx
+++ src/components/settings/SettingsSSOTab/steps/SSOConfigConnectStep.test.jsx
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux';
 import SSOConfigConnectStep from './SSOConfigConnectStep';
 import { SSOConfigContextProvider, SSO_INITIAL_STATE } from '../SSOConfigContext';
 import { getMockStore, initialStore } from '../testutils';
+import LmsApiService from '../../../../data/services/LmsApiService';
 
 const store = getMockStore({ ...initialStore });
 const INITIAL_SSO_STATE = {
@@ -14,6 +15,9 @@ const INITIAL_SSO_STATE = {
     slug: 'slug-provider',
   },
 };
+
+const mockGetProviderConfig = jest.spyOn(LmsApiService, 'getProviderConfig');
+mockGetProviderConfig.mockResolvedValue({ data: { results: 'foobar' } });
 
 describe('SSO Config Connect step', () => {
   test('renders page with metadata link', () => {
