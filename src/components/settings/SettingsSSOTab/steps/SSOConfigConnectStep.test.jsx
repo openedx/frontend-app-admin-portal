666449cc27f59b3827e40811373334e4f2e07e07
Adam Stankiewicz <agstanki@gmail.com>
2022-05-09 13:22:36 -0400

    fix: failing test after rebase

--- src/components/settings/SettingsSSOTab/steps/SSOConfigConnectStep.test.jsx
+++ src/components/settings/SettingsSSOTab/steps/SSOConfigConnectStep.test.jsx
@@ -8,16 +8,18 @@ import { getMockStore, initialStore } from '../testutils';
 import LmsApiService from '../../../../data/services/LmsApiService';
 
 const store = getMockStore({ ...initialStore });
+const TEST_PROVIDER_ID = 'test-provider-id';
 const INITIAL_SSO_STATE = {
   ...SSO_INITIAL_STATE,
   providerConfig: {
+    id: TEST_PROVIDER_ID,
     enterpriseId: 'id-1',
     slug: 'slug-provider',
   },
 };
 
 const mockGetProviderConfig = jest.spyOn(LmsApiService, 'getProviderConfig');
-mockGetProviderConfig.mockResolvedValue({ data: { results: 'foobar' } });
+mockGetProviderConfig.mockResolvedValue({ data: { results: [{ id: TEST_PROVIDER_ID }] } });
 
 describe('SSO Config Connect step', () => {
   test('renders page with metadata link', () => {
