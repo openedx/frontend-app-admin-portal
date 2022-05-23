1ba993ddb927a74c51fcc4ab163822593e078434
Long Lin <lin.longsheng01@gmail.com>
2022-05-23 09:55:28 -0400

    fix: shrink header column in settings access tab

--- src/components/settings/SettingsAccessTab/index.jsx
+++ src/components/settings/SettingsAccessTab/index.jsx
@@ -64,8 +64,8 @@ const SettingsAccessTab = ({
           <h2>Enable browsing on-demand</h2>
         </Col>
       </Row>
-      <Row className="mb-4">
-        <Col>
+      <Row className="mb-4 justify-content-between">
+        <Col lg={8} xl={9}>
           <p>
             Allow learners without a subsidy to browse the catalog and request enrollment to courses.
             Browsing on demand will expire at the end of your latest purchase.
