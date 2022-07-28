6c7853eafd760200565ed3922375d36371672cb1
Abdullah Waheed <abdullah.waheed@arbisoft.com>
2022-07-28 15:10:43 +0500

    refactor: removed paragon Form deprecations from DegreedIntegrationConfigForm

--- src/components/LmsConfigurations/DegreedIntegrationConfigForm.jsx
+++ src/components/LmsConfigurations/DegreedIntegrationConfigForm.jsx
@@ -2,8 +2,9 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import isEmpty from 'lodash/isEmpty';
 import {
-  ValidationFormGroup, Input, StatefulButton, Icon,
+  Form, StatefulButton, Icon,
 } from '@edx/paragon';
+import { Error } from '@edx/paragon/icons';
 import { snakeCaseFormData } from '../../utils';
 import LmsApiService from '../../data/services/LmsApiService';
 import StatusAlert from '../StatusAlert';
@@ -17,6 +18,46 @@ export const REQUIRED_DEGREED_CONFIG_FIELDS = [
   'secret',
   'degreedCompanyId',
   'key',
+];
+
+const DEGREED_FIELDS = [
+  {
+    key: 'degreedCompanyId',
+    invalidMessage: 'Degreed Organization Code is required.',
+    helpText: 'The organization code provided to you by Degreed.',
+    label: 'Degreed Organization Code',
+  },
+  {
+    key: 'degreedBaseUrl',
+    invalidMessage: 'Degreed Instance URL is required.',
+    helpText: 'Your Degreed instance URL. Make sure to include the protocol (ie https/http)',
+    label: 'Degreed Instance URL',
+  },
+  {
+    key: 'key',
+    invalidMessage: 'Degreed API Client ID required.',
+    helpText: 'The API Client ID used to make calls to Degreed on behalf of the customer.',
+    label: 'API Client ID',
+  },
+  {
+    key: 'secret',
+    invalidMessage: 'Degreed API Client Secret required.',
+    helpText: 'The API Client Secret used to make calls to Degreed on behalf of the customer.',
+    label: 'API Client Secret',
+  },
+  {
+    key: 'degreedUserId',
+    invalidMessage: 'The Degreed User ID is required to access Degreed via Oauth.',
+    helpText: 'The Degreed User ID provided to the content provider by Degreed.',
+    label: 'Degreed User ID',
+  },
+  {
+    key: 'degreedUserPassword',
+    invalidMessage: 'The Degreed User Password is required to access Degreed via Oauth.',
+    helpText: 'The Degreed User Password provided to the content provider by Degreed.',
+    label: 'Degreed User Password',
+    type: 'password',
+  },
 ];
 
 function configFormReducer(state, action) {
@@ -184,135 +225,44 @@ function DegreedIntegrationConfigForm({ enterpriseId, config }) {
       </div>
       <div className="row">
         <div className="col col-6">
-          <ValidationFormGroup
-            for="active"
-          >
-            <label htmlFor="active">Active</label>
-            <Input
-              type="checkbox"
+          <Form.Group controlId="active">
+            <Form.Label htmlFor="active">Active</Form.Label>
+            <Form.Checkbox
               id="active"
               name="active"
               className="ml-3"
               checked={active}
               onChange={() => setActive(prevActive => (!prevActive))}
+              isInline
             />
-          </ValidationFormGroup>
+          </Form.Group>
         </div>
       </div>
-      <div className="row">
-        <div className="col col-4">
-          <ValidationFormGroup
-            for="degreedCompanyId"
-            invalid={state.invalidFields.degreedCompanyId}
-            invalidMessage="Degreed Organization Code is required."
-            helpText="The organization code provided to you by Degreed."
-          >
-            <label htmlFor="degreedCompanyId">Degreed Organization Code</label>
-            <Input
-              type="text"
-              id="degreedCompanyId"
-              name="degreedCompanyId"
-              defaultValue={config ? config.degreedCompanyId : null}
-              data-hj-suppress
-            />
-          </ValidationFormGroup>
+      {DEGREED_FIELDS.map(degreedField => (
+        <div className="row" key={degreedField.key}>
+          <div className="col col-4">
+            <Form.Group
+              controlId={degreedField.key}
+              isInvalid={state.invalidFields[degreedField.key]}
+            >
+              <Form.Label htmlFor={degreedField.key}>{degreedField.label}</Form.Label>
+              <Form.Control
+                type={degreedField.type || 'text'}
+                id={degreedField.key}
+                name={degreedField.key}
+                defaultValue={config ? config[degreedField.key] : null}
+                data-hj-suppress
+              />
+              <Form.Text>{degreedField.helpText}</Form.Text>
+              {state.invalidFields[degreedField.key] && degreedField.invalidMessage && (
+              <Form.Control.Feedback icon={<Error className="mr-1" />}>
+                {degreedField.invalidMessage}
+              </Form.Control.Feedback>
+              )}
+            </Form.Group>
+          </div>
         </div>
-      </div>
-      <div className="row">
-        <div className="col col-4">
-          <ValidationFormGroup
-            for="degreedBaseUrl"
-            invalid={state.invalidFields.degreedBaseUrl}
-            invalidMessage="Degreed Instance URL is required."
-            helpText="Your Degreed instance URL. Make sure to include the protocol (ie https/http)"
-          >
-            <label htmlFor="degreedBaseUrl">Degreed Instance URL</label>
-            <Input
-              type="text"
-              id="degreedBaseUrl"
-              name="degreedBaseUrl"
-              defaultValue={config ? config.degreedBaseUrl : null}
-              data-hj-suppress
-            />
-          </ValidationFormGroup>
-        </div>
-      </div>
-      <div className="row">
-        <div className="col col-4">
-          <ValidationFormGroup
-            for="key"
-            invalid={state.invalidFields.key}
-            invalidMessage="Degreed API Client ID required."
-            helpText="The API Client ID used to make calls to Degreed on behalf of the customer."
-          >
-            <label htmlFor="key">API Client ID</label>
-            <Input
-              type="text"
-              id="key"
-              name="key"
-              defaultValue={config ? config.key : null}
-              data-hj-suppress
-            />
-          </ValidationFormGroup>
-        </div>
-      </div>
-      <div className="row">
-        <div className="col col-4">
-          <ValidationFormGroup
-            for="secret"
-            invalid={state.invalidFields.secret}
-            invalidMessage="Degreed API Client Secret required."
-            helpText="The API Client Secret used to make calls to Degreed on behalf of the customer."
-          >
-            <label htmlFor="secret">API Client Secret</label>
-            <Input
-              type="password"
-              id="secret"
-              name="secret"
-              defaultValue={config ? config.secret : null}
-              data-hj-suppress
-            />
-          </ValidationFormGroup>
-        </div>
-      </div>
-      <div className="row">
-        <div className="col col-4">
-          <ValidationFormGroup
-            for="degreedUserId"
-            invalid={state.invalidFields.degreedUserId}
-            invalidMessage="The Degreed User ID is required to access Degreed via Oauth."
-            helpText="The Degreed User ID provided to the content provider by Degreed."
-          >
-            <label htmlFor="degreedUserId">Degreed User ID</label>
-            <Input
-              type="text"
-              id="degreedUserId"
-              name="degreedUserId"
-              defaultValue={config ? config.degreedUserId : null}
-              data-hj-suppress
-            />
-          </ValidationFormGroup>
-        </div>
-      </div>
-      <div className="row">
-        <div className="col col-4">
-          <ValidationFormGroup
-            for="degreedUserPassword"
-            invalid={state.invalidFields.degreedUserPassword}
-            invalidMessage="The Degreed User Password is required to access Degreed via Oauth."
-            helpText="The Degreed User Password provided to the content provider by Degreed."
-          >
-            <label htmlFor="degreedUserPassword">Degreed User Password</label>
-            <Input
-              type="password"
-              id="degreedUserPassword"
-              name="degreedUserPassword"
-              defaultValue={config ? config.degreedUserPassword : null}
-              data-hj-suppress
-            />
-          </ValidationFormGroup>
-        </div>
-      </div>
+      ))}
 
       <div className="row">
         <div className="col col-2">
