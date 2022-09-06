be74ab11faded994cc89115d226b336ebab4d4c4
Hamzah Ullah <hamzahullah@yahoo.com>
2022-09-06 15:37:11 -0400

    feat: Render 404 page on non-existent enterprise-slug

--- src/components/EnterpriseApp/index.jsx
+++ src/components/EnterpriseApp/index.jsx
@@ -12,6 +12,7 @@ import FeatureAnnouncementBanner from '../FeatureAnnouncementBanner';
 import EnterpriseAppContextProvider from './EnterpriseAppContextProvider';
 import EnterpriseAppRoutes from './EnterpriseAppRoutes';
 import ProductTours from '../ProductTours/ProductTours';
+import NotFoundPage from '../NotFoundPage';
 
 class EnterpriseApp extends React.Component {
   constructor(props) {
@@ -125,6 +126,10 @@ class EnterpriseApp extends React.Component {
       return <EnterpriseAppSkeleton />;
     }
 
+    if (!enterpriseId) {
+      return <NotFoundPage />;
+    }
+
     return (
       <EnterpriseAppContextProvider
         enterpriseId={enterpriseId}
@@ -199,7 +204,6 @@ EnterpriseApp.propTypes = {
     url: PropTypes.string.isRequired,
     params: PropTypes.shape({
       enterpriseSlug: PropTypes.string.isRequired,
-      page: PropTypes.string.isRequired,
     }).isRequired,
   }).isRequired,
   enterpriseId: PropTypes.string,
