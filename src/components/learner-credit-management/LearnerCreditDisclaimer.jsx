22685030ba93265820a848fe6654b9ff64fdb431
Hamzah Ullah <hamzahullah@yahoo.com>
2022-09-16 11:59:17 -0400

    feat: Learner Credit Disclaimer message added

---
+++ src/components/learner-credit-management/LearnerCreditDisclaimer.jsx
@@ -0,0 +1,22 @@
+import {
+  Icon, Col, Stack,
+} from '@edx/paragon';
+import { Info } from '@edx/paragon/icons';
+import PropTypes from 'prop-types';
+
+const LearnerCreditDisclaimer = ({ offerLastUpdated }) => (
+  <Stack direction="horizontal" className="mb-4">
+    <Icon src={Info} className="align-self-start" />
+    <Col className="col-8 pl-2">
+      Data last updated on {offerLastUpdated}. This data reflects
+      the current active learner credit only and does not include
+      other spend by your organization (codes, manual enrollments, past offers).
+    </Col>
+  </Stack>
+);
+
+LearnerCreditDisclaimer.propTypes = {
+  offerLastUpdated: PropTypes.string.isRequired,
+};
+
+export default LearnerCreditDisclaimer;
