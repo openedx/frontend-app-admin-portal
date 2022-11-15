dcd2b1745b7e14f49d73b0943b568e8507b9a923
Adam Stankiewicz <agstanki@gmail.com>
2022-11-15 09:34:54 -0500

    chore: update data module

---
+++ src/components/EnterpriseApp/data/hooks/useEnterpriseCuration.js
@@ -0,0 +1,76 @@
+import {
+  useCallback, useEffect, useState,
+} from 'react';
+import { logError } from '@edx/frontend-platform/logging';
+import { camelCaseObject } from '@edx/frontend-platform/utils';
+
+import EnterpriseCatalogApiService from '../../../../data/services/EnterpriseCatalogApiService';
+
+/**
+ * TODO
+ * @param {*} param0
+ * @returns
+ */
+function useEnterpriseCuration({ enterpriseId, curationTitleForCreation }) {
+  const [isLoading, setIsLoading] = useState(true);
+  const [fetchError, setFetchError] = useState(null);
+  const [enterpriseCuration, setEnterpriseCuration] = useState(null);
+
+  const createEnterpriseCuration = useCallback(async () => {
+    try {
+      const response = await EnterpriseCatalogApiService.createEnterpriseCurationConfig(enterpriseId, {
+        title: curationTitleForCreation,
+      });
+      const result = camelCaseObject(response.data);
+      return result;
+    } catch (error) {
+      logError(error);
+      throw error;
+    }
+  }, [enterpriseId, curationTitleForCreation]);
+
+  const getEnterpriseCuration = useCallback(
+    async () => {
+      try {
+        const response = await EnterpriseCatalogApiService.getEnterpriseCurationConfig(enterpriseId);
+        const formattedResponse = camelCaseObject(response.data);
+        const result = formattedResponse.results[0];
+        return result;
+      } catch (error) {
+        logError(error);
+        throw error;
+      }
+    }, [enterpriseId],
+  );
+
+  useEffect(() => {
+    if (!enterpriseId) {
+      setIsLoading(false);
+      return;
+    }
+    const getOrCreateConfiguration = async () => {
+      try {
+        let curation = await getEnterpriseCuration();
+        if (!curation) {
+          curation = await createEnterpriseCuration();
+        }
+        setEnterpriseCuration(curation);
+      } catch (error) {
+        logError(error);
+        setFetchError(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    getOrCreateConfiguration();
+  }, [enterpriseId, getEnterpriseCuration, createEnterpriseCuration]);
+
+  return {
+    isLoading,
+    enterpriseCuration,
+    fetchError,
+  };
+}
+
+export default useEnterpriseCuration;
