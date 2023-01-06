2b813713e8c1716e6e43e8c5c9a099ce04d17665
Adam Stankiewicz <agstanki@gmail.com>
2023-01-06 13:28:29 -0500

    fix: use imported fallback img from @edx/brand

--- src/components/ContentHighlights/HighlightStepper/HighlightStepperTitle.jsx
+++ src/components/ContentHighlights/HighlightStepper/HighlightStepperTitle.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {
   Row, Col, Icon, Container,
 } from '@edx/paragon';
-import { AddCircle } from '@edx/paragon/icons';
+import { EditCircle } from '@edx/paragon/icons';
 
 import { STEPPER_STEP_TEXT } from '../data/constants';
 import HighlightStepperTitleInput from './HighlightStepperTitleInput';
@@ -12,13 +12,13 @@ const HighlightStepperTitle = () => (
     <Row>
       <Col xs={12} md={8} lg={6}>
         <h3 className="mb-3 d-flex align-items-center">
-          <Icon src={AddCircle} className="mr-2 color-brand-tertiary" />
+          <Icon src={EditCircle} className="mr-2 text-brand" />
           {STEPPER_STEP_TEXT.createTitle}
         </h3>
         <div className="mb-4.5">
           <p>
             Create a unique title for your highlight. This title is visible
-            to your learners and helps them discovery relevant content.
+            to your learners and helps them navigate to relevant content.
           </p>
           <p>
             <strong>
