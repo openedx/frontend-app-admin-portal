2a58b349590e1ad69f045603b95aabe55e1a6051
Hamzah Ullah <hamzahullah@yahoo.com>
2023-01-13 09:03:43 -0500

    feat: API integration

--- src/components/ContentHighlights/ContentHighlightCatalogVisibilityRadioInput.jsx
+++ src/components/ContentHighlights/ContentHighlightCatalogVisibilityRadioInput.jsx
@@ -1,4 +1,4 @@
-import { Form, Container } from '@edx/paragon';
+import { Form, Container, Spinner } from '@edx/paragon';
 import { useState, useContext, useEffect } from 'react';
 import { BUTTON_TEXT, LEARNER_PORTAL_CATALOG_VISIBILITY } from './data/constants';
 import { EnterpriseAppContext } from '../EnterpriseApp/EnterpriseAppContextProvider';
@@ -14,9 +14,17 @@ const ContentHighlightCatalogVisibilityRadioInput = () => {
   const [radioGroupVisibility, setRadioGroupVisibility] = useState(true);
   const handleChange = async (e) => {
     e.persist();
-    await updateEnterpriseCuration({
+    if (e.target.dataset.spinnerUi) {
+      document.getElementById(e.target.dataset.spinnerUi).hidden = false;
+      e.target.hidden = true;
+    }
+    const data = await updateEnterpriseCuration({
       canOnlyViewHighlightSets: LEARNER_PORTAL_CATALOG_VISIBILITY[e.target.value].canOnlyViewHighlightSets,
     });
+    if (data) {
+      e.target.hidden = false;
+      document.getElementById(e.target.dataset.spinnerUi).hidden = true;
+    }
     setValue(e.target.value);
   };
   useEffect(() => {
@@ -36,14 +44,32 @@ const ContentHighlightCatalogVisibilityRadioInput = () => {
             value={LEARNER_PORTAL_CATALOG_VISIBILITY.ALL_CONTENT.value}
             type="radio"
             disabled={radioGroupVisibility}
+            data-spinner-ui={`${LEARNER_PORTAL_CATALOG_VISIBILITY.ALL_CONTENT.value}-form-control`}
           >
+            <Spinner
+              hidden
+              id={`${LEARNER_PORTAL_CATALOG_VISIBILITY.ALL_CONTENT.value}-form-control`}
+              size="sm"
+              animation="grow"
+              className="mie-2.5"
+              screenReaderText="loading"
+            />
             {BUTTON_TEXT.catalogVisibilityRadio1}
           </Form.Radio>
           <Form.Radio
             value={LEARNER_PORTAL_CATALOG_VISIBILITY.HIGHLIGHTED_CONTENT.value}
             type="radio"
             disabled={radioGroupVisibility}
+            data-spinner-ui={`${LEARNER_PORTAL_CATALOG_VISIBILITY.HIGHLIGHTED_CONTENT.value}-form-control`}
           >
+            <Spinner
+              hidden
+              id={`${LEARNER_PORTAL_CATALOG_VISIBILITY.HIGHLIGHTED_CONTENT.value}-form-control`}
+              size="sm"
+              animation="grow"
+              className="mie-2.5"
+              screenReaderText="loading"
+            />
             {BUTTON_TEXT.catalogVisibilityRadio2}
           </Form.Radio>
         </Form.RadioSet>
