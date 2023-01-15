b7a95ad84f957570aff862096b82469ad99206ba
zamanafzal <zamanafzal@gmail.com>
2023-01-15 23:49:31 +0500

    feat: Enable sorting for the license table on LPR page

--- src/components/Admin/licenses/LicenseAllocationDetails.jsx
+++ src/components/Admin/licenses/LicenseAllocationDetails.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import LicenseAllocationHeader from './LicenseAllocationHeader';
 import LicenseManagementTable from './LicenseManagementTable';
 
-const LicenseAllocationDetails = () => (
-  <div className="row mb-3">
+const LicenseAllocationDetails = ({ subscriptionUUID }) => (
+  <div className="row mb-3" key={subscriptionUUID}>
     <div className="col">
       <div className="mb-3">
         <LicenseAllocationHeader />
       </div>
-      <LicenseManagementTable />
+      <LicenseManagementTable subscriptionUUID={subscriptionUUID} />
     </div>
   </div>
 );
 
+LicenseAllocationDetails.propTypes = {
+  subscriptionUUID: PropTypes.string.isRequired,
+};
 export default LicenseAllocationDetails;
