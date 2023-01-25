af4ff4a50bf3e2d4ef786a720c9796fb623206d6
Hamzah Ullah <hamzahullah@yahoo.com>
2023-01-25 13:05:58 -0500

    fix: Input reload fix implemented

--- src/components/ContentHighlights/HighlightStepper/HighlightStepperTitleInput.jsx
+++ src/components/ContentHighlights/HighlightStepper/HighlightStepperTitleInput.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useContextSelector } from 'use-context-selector';
 import { Form } from '@edx/paragon';
 
@@ -11,37 +11,59 @@ const HighlightStepperTitleInput = () => {
   const highlightTitle = useContextSelector(ContentHighlightsContext, v => v[0].stepperModal.highlightTitle);
   const [titleLength, setTitleLength] = useState(highlightTitle?.length || 0);
   const [isInvalid, setIsInvalid] = useState(false);
-
-  const handleChange = (e) => {
+  const [highlightValue, setHighlightValue] = useState({
+    initialized: false,
+    highlightTitle: highlightTitle || '',
+    titleStepValidationError: undefined,
+    highlightTitleLength: titleLength || 0,
+  });
+  const handleChange = ((e) => {
+    e.persist();
     if (e.target.value.length > MAX_HIGHLIGHT_TITLE_LENGTH) {
       setIsInvalid(true);
-      setHighlightTitle({
+      setHighlightValue({
+        initialized: true,
         highlightTitle: e.target.value,
         titleStepValidationError: DEFAULT_ERROR_MESSAGE.EXCEEDS_HIGHLIGHT_TITLE_LENGTH,
+        highlightTitleLength: e.target.value.length,
       });
     } else {
       setIsInvalid(false);
-      setHighlightTitle({
+      setHighlightValue({
+        initialized: true,
         highlightTitle: e.target.value,
         titleStepValidationError: undefined,
+        highlightTitleLength: e.target.value.length,
       });
     }
-    setTitleLength(e.target.value.length);
-  };
+  });
 
+  useEffect(() => {
+    if (highlightValue.initialized) {
+      setHighlightTitle({
+        highlightTitle: highlightValue.highlightTitle,
+        titleStepValidationError: highlightValue.titleStepValidationError,
+      });
+      setTitleLength(highlightValue.highlightTitleLength);
+      setHighlightValue({
+        ...highlightValue,
+        initialized: false,
+      });
+    }
+  }, [highlightTitle, setHighlightTitle, highlightValue]);
   return (
     <Form.Group
       isInvalid={isInvalid}
     >
       <Form.Control
         data-testid="stepper-title-input"
-        value={highlightTitle || ''}
+        value={highlightValue.highlightTitle || ''}
         onChange={handleChange}
         floatingLabel="Highlight title"
         autoComplete="off"
       />
       <Form.Control.Feedback type={isInvalid ? 'invalid' : undefined}>
-        {titleLength}/{MAX_HIGHLIGHT_TITLE_LENGTH}
+        {highlightValue.highlightTitleLength}/{MAX_HIGHLIGHT_TITLE_LENGTH}
       </Form.Control.Feedback>
     </Form.Group>
   );
