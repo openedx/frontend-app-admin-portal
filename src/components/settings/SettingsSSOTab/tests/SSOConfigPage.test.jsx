aafbdfcbd5a145470298d3d5fa44cce785305577
Kira Miller <31229189+kiram15@users.noreply.github.com>
2023-02-14 12:05:34 -0700

    feat: adding no sso config card (#970)
    * feat: adding no sso config card
    
    * fix: bug fix
    
    * fix: Update src/components/settings/SettingsSSOTab/index.jsx
    
    Co-authored-by: Marlon Keating <322346+marlonkeating@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Marlon Keating <322346+marlonkeating@users.noreply.github.com>

---
+++ src/components/settings/SettingsSSOTab/tests/SSOConfigPage.test.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+
+import { renderWithRouter } from '../../../test/testUtils';
+import SettingsSSOTab from '../index';
+
+const enterpriseId = 'aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee';
+const enterpriseSlug = 'test-slug';
+const enterpriseName = 'name';
+const enableSamlConfigurationScreen = true;
+const identityProvider = '';
+
+const initialState = {
+  portalConfiguration: {
+    enterpriseId, enterpriseSlug, enterpriseName, enableSamlConfigurationScreen, identityProvider,
+  },
+};
+
+const mockStore = configureMockStore([thunk]);
+const setHasSSOConfig = jest.fn();
+const SettingsSSOWrapper = () => (
+  <Provider store={mockStore({ ...initialState })}>
+    <SettingsSSOTab
+      enterpriseId={enterpriseId}
+      setHasSSOConfig={setHasSSOConfig}
+    />
+  </Provider>
+);
+
+describe('<SettingsSSOTab />', () => {
+  it('Renders with no config card present', async () => {
+    renderWithRouter(<SettingsSSOWrapper />);
+    await waitFor(() => {
+      expect(screen.queryByText('You don\'t have any SSOs integrated yet.')).toBeTruthy();
+      expect(screen.queryByText('New SSO integration')).toBeTruthy();
+      userEvent.click(screen.getByText('New SSO integration'));
+      expect(screen.queryByText('First provide your Identity Provider Metadata and fill out the corresponding fields.')).toBeTruthy();
+    });
+  });
+});
