825025192719f26183dcf78e089ab8eaab0ebb73
Bilal Qamar <59555732+BilalQamar95@users.noreply.github.com>
2023-07-07 15:36:40 +0500

    refactor: updated nvmrc & removed babel.config

--- babel.config.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable import/no-extraneous-dependencies */
-const { getBaseConfig } = require('@edx/frontend-build');
-
-const config = getBaseConfig('babel');
-
-config.presets.push('@babel/preset-typescript');
-
-module.exports = config;
