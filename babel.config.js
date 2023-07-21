70536da79f8803f45d02ca3946529cc06da4cd4e
Bilal Qamar <59555732+BilalQamar95@users.noreply.github.com>
2023-07-21 10:59:25 +0500

    feat: upgraded to node v18, added .nvmrc and updated workflows (#977)
    * feat: upgraded to node v18, added .nvmrc and updated workflows
    
    * refactor: updated packages
    
    * refactor: updated packages
    
    * refactor: updated packages
    
    * refactor: updated frontend-build
    
    * refactor: updated packages
    
    * refactor: updated packages
    
    * refactor: updated packages and resolved typescript issue
    
    * refactor: updated enterprise, platform packages
    
    * refactor: updated packages and snapshots
    
    * refactor: updated packages & lockfileversion workflow
    
    * refactor: updated postcss
    
    * refactor: pinned node version to 18.15
    
    * refactor: reverted frontend-build version & updated packages
    
    * refactor: updated frontend-build
    
    * refactor: updated frontend-build to v12.9.0.alpha.1
    
    * refactor: updated packages and snapshots
    
    * refactor: updated testing-library dom & jest-dom packages
    
    * refactor: updated packages and snapshots
    
    * refactor: updated nvmrc & removed babel.config
    
    ---------
    
    Co-authored-by: Adam Stankiewicz <agstanki@gmail.com>

--- babel.config.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable import/no-extraneous-dependencies */
-const { getBaseConfig } = require('@edx/frontend-build');
-
-const config = getBaseConfig('babel');
-
-module.exports = config;
