86dc7d069e4a2803f98cd596fb5f5ad0514e3291
Adam Stankiewicz <agstanki@gmail.com>
2023-10-05 08:16:11 -0400

    chore: add comments

--- src/components/learner-credit-management/BudgetCard-V2.jsx
+++ src/components/learner-credit-management/BudgetCard-V2.jsx
@@ -4,6 +4,14 @@ import { useOfferSummary } from './data';
 import SubBudgetCard from './SubBudgetCard';
 import { BUDGET_TYPES } from '../EnterpriseApp/data/constants';
 
+/**
+ * Renders one or more budget cards for the given offer (enterprise or Subsidy from enterprise-subsidy). If the offer is
+ * an enterprise offer, it will render a single card. If the offer is a Subsidy, it will render one card for
+ * each associated budget.
+ *
+ * @param {*} offer Represents either an enterprise offer or a Subsidy (enterprise-subsidy).
+ * @returns Budget card component(s).
+ */
 const BudgetCard = ({
   offer,
   enterpriseUUID,
@@ -18,6 +26,7 @@ const BudgetCard = ({
     offerSummary,
   } = useOfferSummary(enterpriseUUID, offer);
 
+  // Enterprise Offers will always have a single budget, so we can render a single card.
   if (offerType === BUDGET_TYPES.ecommerce) {
     return (
       <SubBudgetCard
@@ -33,10 +42,13 @@ const BudgetCard = ({
     );
   }
 
+  // We're now dealing with a Subsidy (enterprise-subsidy), but the analytics API isn't aware of any
+  // associated budgets; nothing should display.
   if (!offerSummary?.budgetsSummary) {
     return null;
   }
 
+  // Render a card for each associated budget with the Subsidy (enterprise-subsidy)
   return offerSummary.budgetsSummary.map((budget) => (
     <SubBudgetCard
       isLoading={isLoadingOfferSummary}
