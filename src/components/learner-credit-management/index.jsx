fc1470b809013cd896d71a7428ad7e9f1c77ee9e
jajjibhai008 <ejazofficial122@gmail.com>
2023-10-12 16:11:51 +0500

    fix: detailpage routes issue

--- src/components/learner-credit-management/index.jsx
+++ src/components/learner-credit-management/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import MultipleBudgetsPage from './MultipleBudgetsPage';
+import './learner-credit.scss';
 import BudgetDetailPage from './BudgetDetailPage';
 
 const LearnerCreditManagementRoutes = ({ match }) => (
