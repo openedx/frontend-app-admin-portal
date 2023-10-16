3aa2937610964d7e9d78183d164a5413d0bf7d00
mashal-m <mashal.malik@arbisoft.com>
2023-10-16 13:47:33 +0500

    fix: fix lint

--- src/components/ContentHighlights/tests/ContentHighlightsCardItemsContainer.test.jsx
+++ src/components/ContentHighlights/tests/ContentHighlightsCardItemsContainer.test.jsx
@@ -1,4 +1,4 @@
-import { screen, waitFor } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 
 import { Provider } from 'react-redux';
