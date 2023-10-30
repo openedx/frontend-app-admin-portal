2c0777edcb641afd3566554472e3610ef1538f37
Adam Stankiewicz <agstanki@gmail.com>
2023-10-30 15:29:33 -0400

    chore: refactor slightly

--- src/components/learner-credit-management/AssignmentsTableRefreshAction.jsx
+++ src/components/learner-credit-management/AssignmentsTableRefreshAction.jsx
@@ -3,22 +3,9 @@ import { Button } from '@edx/paragon';
 import PropTypes from 'prop-types';
 
 const AssignmentsTableRefreshAction = ({ tableInstance, refresh }) => {
-  const { state } = tableInstance;
-
   const handleRefresh = () => {
-    const {
-      pageIndex,
-      pageSize,
-      sortBy,
-      filters,
-    } = state;
-    const dataTableArgs = {
-      pageIndex,
-      pageSize,
-      sortBy,
-      filters,
-    };
-    refresh(dataTableArgs);
+    const { state: dataTableState } = tableInstance;
+    refresh(dataTableState);
   };
 
   return (
@@ -34,18 +21,7 @@ const AssignmentsTableRefreshAction = ({ tableInstance, refresh }) => {
 AssignmentsTableRefreshAction.propTypes = {
   refresh: PropTypes.func.isRequired,
   tableInstance: PropTypes.shape({
-    state: PropTypes.shape({
-      pageIndex: PropTypes.number,
-      pageSize: PropTypes.number,
-      sortBy: PropTypes.arrayOf(PropTypes.shape({
-        id: PropTypes.string,
-        desc: PropTypes.bool,
-      })),
-      filters: PropTypes.arrayOf(PropTypes.shape({
-        id: PropTypes.string,
-        value: PropTypes.string,
-      })),
-    }),
+    state: PropTypes.shape(),
   }),
 };
 
