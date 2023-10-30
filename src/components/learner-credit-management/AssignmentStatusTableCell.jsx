114a3601ec7800e54d4ad9337f1807250759655f
Adam Stankiewicz <agstanki@gmail.com>
2023-10-30 12:46:51 -0400

    feat: add status and recent action columns to assigned table (#1071)

---
+++ src/components/learner-credit-management/AssignmentStatusTableCell.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {
+  Chip,
+} from '@edx/paragon';
+import NotifyingLearner from './assignments-status-chips/NotifyingLearner';
+import WaitingForLearner from './assignments-status-chips/WaitingForLearner';
+import FailedBadEmail from './assignments-status-chips/FailedBadEmail';
+import FailedSystem from './assignments-status-chips/FailedSystem';
+
+const AssignmentStatusTableCell = ({ row }) => {
+  const { original: { learnerEmail, learnerState } } = row;
+
+  // Learner state is not available for this assignment, so don't display anything.
+  if (!learnerState) {
+    return null;
+  }
+
+  if (learnerState === 'notifying') {
+    return (
+      <NotifyingLearner learnerEmail={learnerEmail} />
+    );
+  }
+
+  if (learnerState === 'waiting') {
+    return (
+      <WaitingForLearner learnerEmail={learnerEmail} />
+    );
+  }
+
+  if (learnerState === 'failed') {
+    // Determine the failure reason based on the actions.
+    const { actions } = row.original;
+    const mostRecentAction = actions[0]; // API returns actions in reverse chronological order.
+    const isBadEmailError = mostRecentAction.actionType === 'notified' && !!mostRecentAction.errorReason;
+
+    if (isBadEmailError) {
+      return (
+        <FailedBadEmail learnerEmail={learnerEmail} />
+      );
+    }
+
+    return <FailedSystem />;
+  }
+
+  // Note: The given `learnerState` not officially supported with a `ModalPopup`, but display it anyway.
+  return <Chip>{`${learnerState.charAt(0).toUpperCase()}${learnerState.substr(1)}`}</Chip>;
+};
+
+AssignmentStatusTableCell.propTypes = {
+  row: PropTypes.shape({
+    original: PropTypes.shape({
+      learnerEmail: PropTypes.string,
+      learnerState: PropTypes.string.isRequired,
+      actions: PropTypes.arrayOf(PropTypes.shape({
+        actionType: PropTypes.string.isRequired,
+        errorReason: PropTypes.string,
+      })).isRequired,
+    }).isRequired,
+  }).isRequired,
+};
+
+export default AssignmentStatusTableCell;
