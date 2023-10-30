84f715191103263a594817f67bbe5b9f35182155
Adam Stankiewicz <agstanki@gmail.com>
2023-10-30 19:08:31 -0400

    feat: add assignment modal empty state

---
+++ src/components/learner-credit-management/cards/BaseCourseCard.jsx
@@ -0,0 +1,100 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import {
+  useMediaQuery,
+  breakpoints,
+  Card,
+  Stack,
+  Badge,
+} from '@edx/paragon';
+import { camelCaseObject } from '@edx/frontend-platform/utils';
+
+import useCourseCardMetadata from './data/useCourseCardMetadata';
+import CARD_TEXT from '../constants';
+
+const BaseCourseCard = ({
+  original,
+  footerActions: CardFooterActions,
+  enterpriseSlug,
+  ...rest
+}) => {
+  const isSmall = useMediaQuery({ maxWidth: breakpoints.small.maxWidth });
+  const isExtraSmall = useMediaQuery({ maxWidth: breakpoints.extraSmall.maxWidth });
+  const courseCardMetadata = useCourseCardMetadata({
+    course: camelCaseObject(original),
+    enterpriseSlug,
+  });
+  const {
+    imageSrc,
+    altText,
+    logoSrc,
+    logoAlt,
+    title,
+    subtitle,
+    price,
+    isExecEdCourseType,
+    courseEnrollmentInfo,
+    execEdEnrollmentInfo,
+  } = courseCardMetadata;
+  const { BADGE, PRICE } = CARD_TEXT;
+
+  return (
+    <Card orientation={isSmall ? 'vertical' : 'horizontal'} {...rest}>
+      <Card.ImageCap
+        src={imageSrc}
+        srcAlt={altText}
+        logoSrc={logoSrc}
+        logoAlt={logoAlt}
+      />
+      <Card.Body>
+        <Card.Header
+          title={title}
+          subtitle={subtitle}
+          actions={(
+            <Stack gap={1} className="text-right">
+              <div className="h4 mt-2.5 mb-0">{price}</div>
+              <span className="micro">{PRICE.subText}</span>
+            </Stack>
+          )}
+        />
+        <Card.Section>
+          <Badge variant="light">
+            {isExecEdCourseType ? BADGE.execEd : BADGE.course}
+          </Badge>
+        </Card.Section>
+        <Card.Footer
+          orientation={isExtraSmall ? 'horizontal' : 'vertical'}
+          textElement={isExecEdCourseType ? execEdEnrollmentInfo : courseEnrollmentInfo}
+        >
+          {CardFooterActions && <CardFooterActions {...courseCardMetadata} />}
+        </Card.Footer>
+      </Card.Body>
+    </Card>
+  );
+};
+
+const mapStateToProps = state => ({
+  enterpriseSlug: state.portalConfiguration.enterpriseSlug,
+});
+
+BaseCourseCard.propTypes = {
+  enterpriseSlug: PropTypes.string.isRequired,
+  original: PropTypes.shape({
+    availability: PropTypes.arrayOf(PropTypes.string),
+    cardImageUrl: PropTypes.string,
+    courseType: PropTypes.string,
+    normalizedMetadata: PropTypes.shape(),
+    originalImageUrl: PropTypes.string,
+    partners: PropTypes.arrayOf(
+      PropTypes.shape({
+        logoImageUrl: PropTypes.string,
+        name: PropTypes.string,
+      }),
+    ),
+    title: PropTypes.string,
+  }).isRequired,
+  footerActions: PropTypes.elementType,
+};
+
+export default connect(mapStateToProps)(BaseCourseCard);
--- src/components/learner-credit-management/cards/CourseCard.jsx
+++ src/components/learner-credit-management/cards/CourseCard.jsx
@@ -1,155 +1,41 @@
-/* eslint-disable @typescript-eslint/naming-convention */
-// variables taken from algolia not in camelcase
-import React, { useContext } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import { AppContext } from '@edx/frontend-platform/react';
-import { connect } from 'react-redux';
+import { Button, Hyperlink } from '@edx/paragon';
 
-import {
-  Badge,
-  Button,
-  Card,
-  Stack,
-  Hyperlink,
-  useMediaQuery,
-  breakpoints,
-} from '@edx/paragon';
-import { injectIntl } from '@edx/frontend-platform/i18n';
-import { camelCaseObject } from '@edx/frontend-platform';
-import cardFallbackImg from '@edx/brand/paragon/images/card-imagecap-fallback.png';
-
-import { EXEC_ED_COURSE_TYPE } from '../data';
-import { formatPrice, formatDate, getEnrollmentDeadline } from '../data/utils';
+import NewAssignmentModalButton from '../NewAssignmentModalButton';
 import CARD_TEXT from '../constants';
+import BaseCourseCard from './BaseCourseCard';
 
-const CourseCard = ({
-  original, enterpriseSlug,
-}) => {
-  const {
-    availability,
-    cardImageUrl,
-    courseType,
-    key,
-    normalizedMetadata,
-    partners,
-    title,
-  } = camelCaseObject(original);
+const { BUTTON_ACTION } = CARD_TEXT;
 
-  const { config: { ENTERPRISE_LEARNER_PORTAL_URL } } = useContext(AppContext);
-  const isSmall = useMediaQuery({ maxWidth: breakpoints.small.maxWidth });
-  const isExtraSmall = useMediaQuery({ maxWidth: breakpoints.extraSmall.maxWidth });
+const CourseCardFooterActions = (course) => {
+  const { linkToCourse } = course;
 
-  const {
-    BADGE,
-    BUTTON_ACTION,
-    PRICE,
-    ENROLLMENT,
-  } = CARD_TEXT;
-
-  const price = normalizedMetadata?.contentPrice ? formatPrice(normalizedMetadata.contentPrice, { minimumFractionDigits: 0 }) : 'N/A';
-
-  const imageSrc = cardImageUrl || cardFallbackImg;
-
-  let logoSrc;
-  let logoAlt;
-  if (partners.length === 1) {
-    logoSrc = partners[0]?.logoImageUrl;
-    logoAlt = `${partners[0]?.name}'s logo`;
-  }
-
-  const altText = `${title} course image`;
-
-  const formattedAvailability = availability?.length ? availability.join(', ') : null;
-
-  const enrollmentDeadline = getEnrollmentDeadline(normalizedMetadata?.enrollByDate);
-
-  let courseEnrollmentInfo;
-  let execEdEnrollmentInfo;
-  if (normalizedMetadata?.enrollByDate) {
-    courseEnrollmentInfo = `${formattedAvailability} • ${ENROLLMENT.text} ${enrollmentDeadline}`;
-    execEdEnrollmentInfo = `Starts ${formatDate(normalizedMetadata.startDate)} •
-    ${ENROLLMENT.text} ${enrollmentDeadline}`;
-  } else {
-    courseEnrollmentInfo = formattedAvailability;
-    execEdEnrollmentInfo = formattedAvailability;
-  }
-
-  const isExecEd = courseType === EXEC_ED_COURSE_TYPE;
-
-  let linkToCourse;
-  if (isExecEd) {
-    linkToCourse = `${ENTERPRISE_LEARNER_PORTAL_URL}/${enterpriseSlug}/executive-education-2u/course/${key}`;
-  } else {
-    linkToCourse = `${ENTERPRISE_LEARNER_PORTAL_URL}/${enterpriseSlug}/course/${key}`;
-  }
-
-  return (
-    <Card
-      orientation={isSmall ? 'vertical' : 'horizontal'}
+  return [
+    <Button
+      as={Hyperlink}
+      data-testid="hyperlink-view-course"
+      destination={linkToCourse}
+      target="_blank"
+      variant="outline-primary"
     >
-      <Card.ImageCap
-        src={imageSrc}
-        srcAlt={altText}
-        logoSrc={logoSrc}
-        logoAlt={logoAlt}
-      />
-      <Card.Body>
-        <Card.Header
-          title={title}
-          className="mb-0 mt-0"
-          subtitle={partners[0]?.name}
-          actions={(
-            <Stack gap={1} className="text-right">
-              <p className="h4 mt-2.5 mb-0">{price}</p>
-              <span className="micro">{PRICE.subText}</span>
-            </Stack>
-          )}
-        />
-        <Card.Section>
-          <Badge variant="light">
-            {isExecEd ? BADGE.execEd : BADGE.course}
-          </Badge>
-        </Card.Section>
-        <Card.Footer
-          orientation={isExtraSmall ? 'horizontal' : 'vertical'}
-          textElement={isExecEd ? execEdEnrollmentInfo : courseEnrollmentInfo}
-        >
-          <Button
-            as={Hyperlink}
-            data-testid="hyperlink-view-course"
-            destination={linkToCourse}
-            target="_blank"
-            variant="outline-primary"
-          >
-            {BUTTON_ACTION.viewCourse}
-          </Button>
-          <Button>{BUTTON_ACTION.assign}</Button>
-        </Card.Footer>
-      </Card.Body>
-    </Card>
-  );
+      {BUTTON_ACTION.viewCourse}
+    </Button>,
+    <NewAssignmentModalButton course={course}>
+      {BUTTON_ACTION.assign}
+    </NewAssignmentModalButton>,
+  ];
 };
+
+const CourseCard = ({ original }) => (
+  <BaseCourseCard
+    original={original}
+    footerActions={CourseCardFooterActions}
+  />
+);
 
 CourseCard.propTypes = {
-  enterpriseSlug: PropTypes.string.isRequired,
-  original: PropTypes.shape({
-    availability: PropTypes.arrayOf(PropTypes.string),
-    cardImageUrl: PropTypes.string,
-    courseType: PropTypes.string,
-    normalizedMetadata: PropTypes.shape(),
-    originalImageUrl: PropTypes.string,
-    partners: PropTypes.arrayOf(
-      PropTypes.shape({
-        logoImageUrl: PropTypes.string,
-        name: PropTypes.string,
-      }),
-    ),
-    title: PropTypes.string,
-  }).isRequired,
+  original: PropTypes.shape().isRequired, // pass-thru prop to `BaseCourseCard`
 };
 
-const mapStateToProps = state => ({
-  enterpriseSlug: state.portalConfiguration.enterpriseSlug,
-});
-
-export default connect(mapStateToProps)(injectIntl(CourseCard));
+export default CourseCard;
