c9ff1e7903846ec5638edd6d9114c4cd96a6a320
Alexander J Sheehan <asheehan@2u.com>
2023-11-07 20:24:35 +0000

    feat: implementing gated top down allocation table subsidy service api usage

--- src/components/learner-credit-management/data/hooks/useOfferRedemptions.js
+++ src/components/learner-credit-management/data/hooks/useOfferRedemptions.js
@@ -10,8 +10,10 @@ import { sendEnterpriseTrackEvent } from '@edx/frontend-enterprise-utils';
 import debounce from 'lodash.debounce';
 
 import EnterpriseDataApiService from '../../../../data/services/EnterpriseDataApiService';
+import SubsidyApiService from '../../../../data/services/EnterpriseSubsidyApiService';
 import { API_FIELDS_BY_TABLE_COLUMN_ACCESSOR } from '../constants';
-import { transformUtilizationTableResults } from '../utils';
+import { transformUtilizationTableResults, transformUtilizationTableSubsidyTransactionResults } from '../utils';
+import useSubsidyAccessPolicy from './useSubsidyAccessPolicy';
 
 const applySortByToOptions = (sortBy, options) => {
   const orderingStrings = sortBy.map(({ id, desc }) => {
@@ -29,19 +31,26 @@ const applySortByToOptions = (sortBy, options) => {
   });
 };
 
-const applyFiltersToOptions = (filters, options) => {
+const applyFiltersToOptions = (filters, options, shouldFetchSubsidyTransactions = false) => {
   const courseProductLineSearchQuery = filters?.find(filter => filter.id === 'courseProductLine')?.value;
-  const searchQuery = filters?.find(filter => filter.id.toLowerCase() === 'enrollment details')?.value;
+  const searchQuery = filters?.find(filter => filter.id === 'enrollmentDetails')?.value;
 
   if (courseProductLineSearchQuery) {
     Object.assign(options, { courseProductLine: courseProductLineSearchQuery });
   }
   if (searchQuery) {
-    Object.assign(options, { searchAll: searchQuery });
+    const searchParams = {};
+    searchParams[shouldFetchSubsidyTransactions ? 'search' : 'searchAll'] = searchQuery;
+    Object.assign(options, searchParams);
   }
 };
 
-const useOfferRedemptions = (enterpriseUUID, offerId = null, budgetId = null) => {
+const useOfferRedemptions = (
+  enterpriseUUID,
+  offerId = null,
+  budgetId = null,
+  shouldFetchSubsidyTransactions = false,
+) => {
   const shouldTrackFetchEvents = useRef(false);
   const [isLoading, setIsLoading] = useState(true);
   const [offerRedemptions, setOfferRedemptions] = useState({
@@ -49,6 +58,7 @@ const useOfferRedemptions = (enterpriseUUID, offerId = null, budgetId = null) =>
     pageCount: 0,
     results: [],
   });
+  const { data: subsidyAccessPolicy } = useSubsidyAccessPolicy(budgetId);
 
   const fetchOfferRedemptions = useCallback((args) => {
     const fetch = async () => {
@@ -69,14 +79,26 @@ const useOfferRedemptions = (enterpriseUUID, offerId = null, budgetId = null) =>
           applySortByToOptions(args.sortBy, options);
         }
         if (args.filters?.length > 0) {
-          applyFiltersToOptions(args.filters, options);
+          applyFiltersToOptions(args.filters, options, shouldFetchSubsidyTransactions);
         }
-        const response = await EnterpriseDataApiService.fetchCourseEnrollments(
-          enterpriseUUID,
-          options,
-        );
-        const data = camelCaseObject(response.data);
-        const transformedTableResults = transformUtilizationTableResults(data.results);
+        let data;
+        let transformedTableResults;
+        if (budgetId && shouldFetchSubsidyTransactions) {
+          const response = await SubsidyApiService.fetchCustomerTransactions(
+            subsidyAccessPolicy?.subsidyUuid,
+            options,
+          );
+          data = camelCaseObject(response.data);
+          transformedTableResults = transformUtilizationTableSubsidyTransactionResults(data.results);
+        } else {
+          const response = await EnterpriseDataApiService.fetchCourseEnrollments(
+            enterpriseUUID,
+            options,
+          );
+          data = camelCaseObject(response.data);
+          transformedTableResults = transformUtilizationTableResults(data.results);
+        }
+
         setOfferRedemptions({
           itemCount: data.count,
           pageCount: data.numPages,
@@ -104,7 +126,14 @@ const useOfferRedemptions = (enterpriseUUID, offerId = null, budgetId = null) =>
     if (offerId || budgetId) {
       fetch();
     }
-  }, [enterpriseUUID, offerId, budgetId, shouldTrackFetchEvents]);
+  }, [
+    enterpriseUUID,
+    offerId,
+    budgetId,
+    shouldTrackFetchEvents,
+    shouldFetchSubsidyTransactions,
+    subsidyAccessPolicy?.subsidyUuid,
+  ]);
 
   const debouncedFetchOfferRedemptions = useMemo(() => debounce(fetchOfferRedemptions, 300), [fetchOfferRedemptions]);
 
