126cb6eea0e74671d90ff4a33f7bc98df7edeb95
Adam Stankiewicz <agstanki@gmail.com>
2023-11-08 18:37:15 -0500

    chore: addtl coverage for filtering and ordering on useBudgetContentAssignments

--- src/components/learner-credit-management/data/hooks/useBudgetContentAssignments.test.js
+++ src/components/learner-credit-management/data/hooks/useBudgetContentAssignments.test.js
@@ -128,4 +128,156 @@ describe('useBudgetContentAssignments', () => {
       },
     );
   });
+
+  it.each([
+    {
+      filters: [
+        {
+          id: 'learnerState',
+          value: ['waiting'],
+        },
+      ],
+      selectedLearnerStateQueryParam: 'waiting',
+    },
+    {
+      filters: [
+        {
+          id: 'learnerState',
+          value: ['waiting', 'notifying'],
+        },
+      ],
+      selectedLearnerStateQueryParam: 'waiting,notifying',
+    },
+  ])('handles learner state (status) filtering (%s)', async ({ filters, selectedLearnerStateQueryParam }) => {
+    const { result, waitForNextUpdate } = renderHook(() => useBudgetContentAssignments({
+      assignmentConfigurationUUID: '123',
+      isEnabled: true,
+    }));
+    const { fetchContentAssignments } = result.current;
+    const mockListContentAssignments = jest.spyOn(EnterpriseAccessApiService, 'listContentAssignments');
+    mockListContentAssignments.mockResolvedValue({
+      data: {
+        results: [
+          {
+            id: 1,
+            name: 'test',
+          },
+        ],
+        count: 1,
+        numPages: 1,
+        currentPage: 1,
+      },
+    });
+    await fetchContentAssignments({
+      pageIndex: 0,
+      pageSize: 10,
+      filters,
+    });
+
+    await waitForNextUpdate();
+
+    expect(mockListContentAssignments).toHaveBeenCalledWith(
+      '123',
+      {
+        page: 1,
+        pageSize: 10,
+        learnerState: selectedLearnerStateQueryParam,
+      },
+    );
+  });
+
+  it.each([
+    {
+      sortBy: [
+        {
+          id: 'learnerState',
+          desc: false,
+        },
+      ],
+      orderingQueryParam: 'learner_state_sort_order',
+    },
+    {
+      sortBy: [
+        {
+          id: 'learnerState',
+          desc: true,
+        },
+      ],
+      orderingQueryParam: '-learner_state_sort_order',
+    },
+    {
+      sortBy: [
+        {
+          id: 'recentAction',
+          desc: false,
+        },
+      ],
+      orderingQueryParam: 'recent_action_time',
+    },
+    {
+      sortBy: [
+        {
+          id: 'recentAction',
+          desc: true,
+        },
+      ],
+      orderingQueryParam: '-recent_action_time',
+    },
+    {
+      sortBy: [
+        {
+          id: 'amount',
+          desc: false,
+        },
+      ],
+      // Ordering is reversed for `content_quantity` field
+      orderingQueryParam: '-content_quantity',
+    },
+    {
+      sortBy: [
+        {
+          id: 'amount',
+          desc: true,
+        },
+      ],
+      // Ordering is reversed for `content_quantity` field
+      orderingQueryParam: 'content_quantity',
+    },
+  ])('handles ordering on appropriate columns (%s)', async ({ sortBy, orderingQueryParam }) => {
+    const { result, waitForNextUpdate } = renderHook(() => useBudgetContentAssignments({
+      assignmentConfigurationUUID: '123',
+      isEnabled: true,
+    }));
+    const { fetchContentAssignments } = result.current;
+    const mockListContentAssignments = jest.spyOn(EnterpriseAccessApiService, 'listContentAssignments');
+    mockListContentAssignments.mockResolvedValue({
+      data: {
+        results: [
+          {
+            id: 1,
+            name: 'test',
+          },
+        ],
+        count: 1,
+        numPages: 1,
+        currentPage: 1,
+      },
+    });
+    await fetchContentAssignments({
+      pageIndex: 0,
+      pageSize: 10,
+      sortBy,
+    });
+
+    await waitForNextUpdate();
+
+    expect(mockListContentAssignments).toHaveBeenCalledWith(
+      '123',
+      {
+        page: 1,
+        pageSize: 10,
+        ordering: orderingQueryParam,
+      },
+    );
+  });
 });
