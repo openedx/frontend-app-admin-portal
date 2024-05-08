a61457f9ee7216137c0a2ebe4507ea1ccec4548a
irfanuddinahmad <34648393+irfanuddinahmad@users.noreply.github.com>
2024-05-08 14:07:39 +0500

    feat: Added new highlights stepper i18n text (#1209)

--- src/components/ContentHighlights/HighlightStepper/HighlightStepperTitle.jsx
+++ src/components/ContentHighlights/HighlightStepper/HighlightStepperTitle.jsx
@@ -3,8 +3,8 @@ import {
   Row, Col, Icon, Container,
 } from '@edx/paragon';
 import { EditCircle } from '@edx/paragon/icons';
+import { FormattedMessage } from '@edx/frontend-platform/i18n';
 
-import { STEPPER_STEP_TEXT } from '../data/constants';
 import HighlightStepperTitleInput from './HighlightStepperTitleInput';
 
 const HighlightStepperTitle = () => (
@@ -13,15 +13,34 @@ const HighlightStepperTitle = () => (
       <Col xs={12} md={8} lg={6}>
         <h3 className="mb-3 d-flex align-items-center">
           <Icon src={EditCircle} className="mr-2 text-brand" />
-          {STEPPER_STEP_TEXT.HEADER_TEXT.createTitle}
+          <FormattedMessage
+            id="highlights.new.highlights.stepper.stepper.step.header.text.create.title"
+            defaultMessage="Create a title for your highlight"
+            description="Create title header message shown to administrators during creation of new content highlights"
+          />
         </h3>
         <div className="mb-4.5">
           <p>
-            {STEPPER_STEP_TEXT.SUB_TEXT.createTitle}
+            <FormattedMessage
+              id="highlights.new.highlights.stepper.stepper.step.sub.text.create.title"
+              defaultMessage="Create a unique title for your highlight. This title is visible
+              to your learners and helps them navigate to relevant content."
+              description="Create title sub message shown to administrators during creation of new content highlights"
+            />
           </p>
           <p>
             <strong>
-              {STEPPER_STEP_TEXT.PRO_TIP_TEXT.createTitle}
+              <FormattedMessage
+                id="highlights.new.highlights.stepper.stepper.step.pro.tip.text.create.title"
+                defaultMessage="Pro tip: we recommend naming your highlight collection to reflect skills
+                it aims to develop, or to draw the attention of specific groups it targets.
+                For example, {doubleQoute}Recommended for Marketing{doubleQoute}
+                 or {doubleQoute}Develop Leadership Skills{doubleQoute}."
+                description="Create title pro tip message shown to administrators during creation of new content highlights"
+                values={{
+                  doubleQoute: '"',
+                }}
+              />
             </strong>
           </p>
         </div>
