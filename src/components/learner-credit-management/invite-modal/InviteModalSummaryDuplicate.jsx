0e6de58668ae7aca66365b7d8ea3407d777b7fcf
Katrina Nguyen <71999631+katrinan029@users.noreply.github.com>
2024-05-28 10:44:18 -0700

    fix: update info icon color (#1238)

--- src/components/learner-credit-management/invite-modal/InviteModalSummaryDuplicate.jsx
+++ src/components/learner-credit-management/invite-modal/InviteModalSummaryDuplicate.jsx
@@ -4,7 +4,7 @@ import { Error } from '@openedx/paragon/icons';
 
 const InviteModalSummaryDuplicate = () => (
   <Stack className="duplicate-warning" direction="horizontal" gap={3}>
-    <Icon className="text-info" src={Error} />
+    <Icon className="text-info-500" src={Error} />
     <div>
       <div className="h4 mb-1">Only 1 invite per email address will be sent.</div>
       <span className="small">One or more duplicate emails were detected. Ensure that your entry is correct before proceeding.</span>
