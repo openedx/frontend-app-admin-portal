69f411736534e6d648fad0ab958b75c0b260c1d3
Cyril Nxumalo <cnxumalo@2u.com>
2024-08-30 10:36:43 +0200

    chore: Migrate deprecated Table to DataTable for RegisteredLearnersTable

--- src/components/RegisteredLearnersTable/index.jsx
+++ src/components/RegisteredLearnersTable/index.jsx
@@ -1,54 +1,77 @@
 import React from 'react';
-
+import PropTypes from 'prop-types';
 import { useIntl } from '@edx/frontend-platform/i18n';
-
-import TableContainer from '../../containers/TableContainer';
-
+import { DataTable } from '@openedx/paragon';
+import { connect } from 'react-redux';
+import useRegisteredLearners from './data/hooks/useRegisteredLearners';
 import { i18nFormatTimestamp } from '../../utils';
-import EnterpriseDataApiService from '../../data/services/EnterpriseDataApiService';
 
-const RegisteredLearnersTable = () => {
+const UserEmail = ({ row }) => (
+  <span data-hj-suppress>{row.original.userEmail}</span>
+);
+
+UserEmail.propTypes = {
+  row: PropTypes.shape({
+    original: PropTypes.shape({
+      userEmail: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
+};
+
+const RegisteredLearnersTable = ({ enterpriseId }) => {
   const intl = useIntl();
-
-  const tableColumns = [
-    {
-      label: intl.formatMessage({
-        id: 'admin.portal.lpr.registered.learners.table.user_email.column.heading',
-        defaultMessage: 'Email',
-        description: 'Column heading for the user email column in the registered learners table',
-      }),
-      key: 'user_email',
-      columnSortable: true,
-    },
-    {
-      label: intl.formatMessage({
-        id: 'admin.portal.lpr.registered.learners.table.lms_user_created.column.heading',
-        defaultMessage: 'Account Created',
-        description: 'Column heading for the lms user created column in the registered learners table',
-      }),
-      key: 'lms_user_created',
-      columnSortable: true,
-    },
-  ];
-
-  const formatLearnerData = learners => learners.map(learner => ({
-    ...learner,
-    user_email: <span data-hj-suppress>{learner.user_email}</span>,
-    lms_user_created: i18nFormatTimestamp({
-      intl, timestamp: learner.lms_user_created,
-    }),
-  }));
+  const {
+    isLoading,
+    registeredLearners: tableData,
+    fetchRegisteredLearners: fetchTableData,
+  } = useRegisteredLearners(enterpriseId);
 
   return (
-    <TableContainer
-      id="registered-unenrolled-learners"
-      className="registered-unenrolled-learners"
-      fetchMethod={EnterpriseDataApiService.fetchUnenrolledRegisteredLearners}
-      columns={tableColumns}
-      formatData={formatLearnerData}
-      tableSortable
+    <DataTable
+      isSortable
+      manualSortBy
+      isPaginated
+      manualPagination
+      isLoading={isLoading}
+      columns={[
+        {
+          Header: intl.formatMessage({
+            id: 'admin.portal.lpr.registered.learners.table.user_email.column.heading',
+            defaultMessage: 'Email',
+            description: 'Column heading for the user email column in the registered learners table',
+          }),
+          accessor: 'userEmail',
+          Cell: UserEmail,
+        },
+        {
+          Header: intl.formatMessage({
+            id: 'admin.portal.lpr.registered.learners.table.lms_user_created.column.heading',
+            defaultMessage: 'Account Created',
+            description: 'Column heading for the lms user created column in the registered learners table',
+          }),
+          accessor: 'lmsUserCreated',
+          Cell: ({ row }) => i18nFormatTimestamp({ intl, timestamp: row.values.lmsUserCreated }),
+        },
+      ]}
+      initialState={{
+        pageSize: 20, // Set this according to your requirements
+        pageIndex: 0,
+        sortBy: [{ id: 'lmsUserCreated', desc: true }],
+      }}
+      fetchData={fetchTableData}
+      data={tableData.results}
+      itemCount={tableData.itemCount}
+      pageCount={tableData.pageCount}
     />
   );
 };
 
-export default RegisteredLearnersTable;
+const mapStateToProps = state => ({
+  enterpriseId: state.portalConfiguration.enterpriseId,
+});
+
+RegisteredLearnersTable.propTypes = {
+  enterpriseId: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps)(RegisteredLearnersTable);
