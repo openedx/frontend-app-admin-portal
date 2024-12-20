e6110115cf200b885645246186935013704fd693
Marlon Keating <mkeating@2u.com>
2024-12-20 15:38:47 +0000

    feat: Add download csv button to people management
    test: Add DownloadCsvButton unit test
    
    refactor: move getTimeStampedFilename into utils

---
+++ src/components/PeopleManagement/DownloadCSVButton.jsx
@@ -0,0 +1,105 @@
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { useIntl } from '@edx/frontend-platform/i18n';
+
+import {
+  Icon, Spinner, StatefulButton, Toast, useToggle,
+} from '@openedx/paragon';
+import { Download, Check } from '@openedx/paragon/icons';
+import { logError } from '@edx/frontend-platform/logging';
+import { downloadCsv, getTimeStampedFilename } from '../../utils';
+
+const csvHeaders = ['Name', 'Email', 'Joined Organization', 'Enrollments'];
+
+const dataEntryToRow = (entry) => {
+  const { enterpriseCustomerUser: { name, email, joinedOrg }, enrollments } = entry;
+  return [name, email, joinedOrg, enrollments];
+};
+
+const DownloadCsvButton = ({ testId, fetchData, totalCt }) => {
+  const [buttonState, setButtonState] = useState('pageLoading');
+  const [isToastOpen, openToast, closeToast] = useToggle(false);
+  const intl = useIntl();
+
+  useEffect(() => {
+    if (fetchData) {
+      setButtonState('default');
+    }
+  }, [fetchData]);
+
+  const handleClick = async () => {
+    setButtonState('pending');
+    fetchData().then((response) => {
+      const fileName = getTimeStampedFilename('people-report.csv');
+      downloadCsv(fileName, response.results, csvHeaders, dataEntryToRow);
+      openToast();
+      setButtonState('complete');
+    }).catch((err) => {
+      logError(err);
+    });
+  };
+
+  const toastText = intl.formatMessage({
+    id: 'adminPortal.peopleManagement.dataTable.download.toast',
+    defaultMessage: 'Successfully downloaded',
+    description: 'Toast message for the people management download button.',
+  });
+  return (
+    <>
+      { isToastOpen
+     && (
+     <Toast onClose={closeToast} show={isToastOpen}>
+       {toastText}
+     </Toast>
+     )}
+      <StatefulButton
+        state={buttonState}
+        className="download-button"
+        data-testid={testId}
+        labels={{
+          default: intl.formatMessage({
+            id: 'adminPortal.peopleManagement.dataTable.download.button',
+            defaultMessage: `Download all (${totalCt})`,
+            description: 'Label for the people management download button',
+          }),
+          pending: intl.formatMessage({
+            id: 'adminPortal.peopleManagement.dataTable.download.button.pending',
+            defaultMessage: 'Downloading',
+            description: 'Label for the people management download button when the download is in progress.',
+          }),
+          complete: intl.formatMessage({
+            id: 'adminPortal.peopleManagement.dataTable.download.button.complete',
+            defaultMessage: 'Downloaded',
+            description: 'Label for the people management download button when the download is complete.',
+          }),
+          pageLoading: intl.formatMessage({
+            id: 'adminPortal.peopleManagement.dataTable.download.button.loading',
+            defaultMessage: 'Download module activity',
+            description: 'Label for the people management download button when the page is loading.',
+          }),
+        }}
+        icons={{
+          default: <Icon src={Download} />,
+          pending: <Spinner animation="border" variant="light" size="sm" />,
+          complete: <Icon src={Check} />,
+          pageLoading: <Icon src={Download} variant="light" />,
+        }}
+        disabledStates={['pending', 'pageLoading']}
+        onClick={handleClick}
+      />
+    </>
+  );
+};
+
+DownloadCsvButton.defaultProps = {
+  testId: 'download-csv-button',
+};
+
+DownloadCsvButton.propTypes = {
+  // eslint-disable-next-line react/forbid-prop-types
+  fetchData: PropTypes.func.isRequired,
+  totalCt: PropTypes.number,
+  testId: PropTypes.string,
+};
+
+export default DownloadCsvButton;
