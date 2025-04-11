f942ce0e351466742b41732519b9a1c4696edb99
Katrina Nguyen <71999631+katrinan029@users.noreply.github.com>
2025-04-11 15:20:53 -0700

    feat: adds zero state to learner profile (#1474)
    * feat: adds zero state to learner profile
    
    * chore: refactored based on feedback

--- src/components/PeopleManagement/LearnerDetailPage/CourseEnrollments.jsx
+++ src/components/PeopleManagement/LearnerDetailPage/CourseEnrollments.jsx
@@ -1,33 +1,48 @@
+import React from 'react';
 import PropTypes from 'prop-types';
-import { Skeleton } from '@openedx/paragon';
+import { useIntl } from '@edx/frontend-platform/i18n';
+import { Skeleton, Card } from '@openedx/paragon';
 import EnrollmentCard from './EnrollmentCard';
 
-const CourseEnrollments = ({ enrollments, isLoading }) => (
-  <div className="ml-5">
-    {isLoading && !enrollments ? (
-      <Skeleton
-        width={400}
-        height={200}
-      />
-    ) : (
-      <>
-        <h3>Enrollments</h3>
-        {enrollments?.inProgress?.map((enrollment) => (
-          <EnrollmentCard enrollment={enrollment} />
-        ))}
-        {enrollments?.upcoming?.map((enrollment) => (
-          <EnrollmentCard enrollment={enrollment} />
-        ))}
-        {enrollments?.completed?.map((enrollment) => (
-          <EnrollmentCard enrollment={enrollment} />
-        ))}
-        {enrollments.assignmentsForDisplay.map((enrollment) => (
-          <EnrollmentCard enrollment={enrollment} />
-        ))}
-      </>
-    )}
-  </div>
-);
+const CourseEnrollments = ({ enrollments, isLoading }) => {
+  const intl = useIntl();
+  const noEnrollmentsMessage = intl.formatMessage({
+    id: 'adminPortal.peopleManagement.learnerDetailPage.noEnrollmentsMessage',
+    defaultMessage: 'This learner has not enrolled in any courses.',
+    description: 'Message displayed when a learner has no course enrollments',
+  });
+
+  const enrollmentTypes = ['inProgress', 'upcoming', 'completed', 'assignmentsForDisplay'];
+  const flattenedEnrollments = enrollmentTypes.flatMap(type => enrollments?.[type] || []);
+  const hasEnrollments = flattenedEnrollments.length > 0;
+
+  if (isLoading) {
+    return (
+      <div className="ml-5">
+        <Skeleton width={400} height={200} data-testid="skeleton" />
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <h3>Enrollments</h3>
+      {hasEnrollments ? (
+        <>
+          {flattenedEnrollments.map((enrollment) => (
+            <EnrollmentCard key={enrollment.uuid} enrollment={enrollment} />
+          ))}
+        </>
+      ) : (
+        <Card className="mb-4">
+          <Card.Section>
+            <p className="text-muted mb-0 pt-3 pb-3 pl-3 ">{noEnrollmentsMessage}</p>
+          </Card.Section>
+        </Card>
+      )}
+    </>
+  );
+};
 
 const enrollmentShape = PropTypes.shape({
   courseKey: PropTypes.string,
