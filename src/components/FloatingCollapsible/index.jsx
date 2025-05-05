c637bc18919d2396c137d9f163f9eb5c8dac0e92
Jesper Hodge <jhodge@outlook.de>
2025-05-05 17:30:25 -0400

    feat: use brand color for collapsible

--- src/components/FloatingCollapsible/index.jsx
+++ src/components/FloatingCollapsible/index.jsx
@@ -1,16 +1,30 @@
 import React from 'react';
-
+import { connect } from 'react-redux';
 import { Collapsible } from '@openedx/paragon';
+import Color from 'color';
 
-const FloatingCollapsible = () => (
-  <div className="bottom-right-fixed">
-    <Collapsible
-      styling="card"
-      title="Product Tours Checklist"
-    >
-      <div>Product Tours Checklist</div>
-    </Collapsible>
-  </div>
-);
+const FloatingCollapsible = ({ enterpriseBranding }) => {
+  // Use the bottom-right-fixed class that's already defined in index.scss
+  const collapsibleStyle = enterpriseBranding?.tertiary_color ? {
+    backgroundColor: enterpriseBranding.tertiary_color,
+    color: enterpriseBranding.tertiary_color && Color(enterpriseBranding.tertiary_color).isDark() ? '#FFFFFF' : '#454545',
+  } : {};
 
-export default FloatingCollapsible;
+  return (
+    <div className="bottom-right-fixed">
+      <Collapsible
+        styling="card"
+        title="Product Tours Checklist"
+        style={collapsibleStyle}
+      >
+        <div>Product Tours Checklist</div>
+      </Collapsible>
+    </div>
+  );
+};
+
+const mapStateToProps = state => ({
+  enterpriseBranding: state.portalConfiguration.enterpriseBranding,
+});
+
+export default connect(mapStateToProps)(FloatingCollapsible);
