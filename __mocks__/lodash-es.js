b5e42bbb42797e4af167b9dbf4ffa141839caae2
Hamzah Ullah <hamzahullah@yahoo.com>
2025-05-21 11:18:18 -0400

    chore: create a mock file for lodash-es

---
+++ __mocks__/lodash-es.js
@@ -0,0 +1,25 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+const lodash = require('lodash');
+
+module.exports = {
+  debounce: lodash.debounce,
+  snakeCase: lodash.snakeCase,
+  isEmpty: lodash.isEmpty,
+  isEqual: lodash.isEqual,
+  cloneDeep: lodash.cloneDeep,
+  omit: lodash.omit,
+  omitBy: lodash.omitBy,
+  groupBy: lodash.groupBy,
+  union: lodash.union,
+  partition: lodash.partition,
+  keys: lodash.keys,
+  last: lodash.last,
+  range: lodash.range,
+  isString: lodash.isString,
+  camelCase: lodash.camelCase,
+  fromPairs: lodash.fromPairs,
+  isArray: lodash.isArray,
+  isNumber: lodash.isNumber,
+  mergeWith: lodash.mergeWith,
+  without: lodash.without,
+};
--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,12 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 const { createConfig } = require('@openedx/frontend-build');
+const path = require('path');
 
 module.exports = createConfig('jest', {
-  transform: {
-    '^.+\\.[jt]sx?$': 'babel-jest',
-  },
   setupFiles: [
     '<rootDir>/src/setupTest.js',
   ],
-  transformIgnorePatterns: [
-    '/node_modules/(?!(lodash-es|@(open)?edx)/)',
-  ],
+  moduleNameMapper: {
+    '^lodash-es$': path.resolve(__dirname, '__mocks__/lodash-es.js'),
+  },
 });
