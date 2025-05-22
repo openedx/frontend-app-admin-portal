90cc436855098ca00132a41c61fdef49cebd8510
Hamzah Ullah <hamzahullah@yahoo.com>
2025-05-22 09:18:37 -0400

    chore: PR feedback

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,6 +1,6 @@
 const {createConfig} = require('@openedx/frontend-build');
 
-const config = createConfig('dev', {})
+const config = createConfig('webpack-dev')
 
 config.module.rules[0].exclude = /node_modules\/(?!(lodash-es|@(open)?edx))/
 
