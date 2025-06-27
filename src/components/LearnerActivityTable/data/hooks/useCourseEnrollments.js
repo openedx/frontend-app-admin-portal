ac4d6d0ca6164a40e54fd71a237f7b5d7abe518c
muhammad-ammar <mammar@gmail.com>
2025-06-27 11:04:23 +0500

    fix: datatable data fixes

--- src/components/LearnerActivityTable/data/hooks/useCourseEnrollments.js
+++ src/components/LearnerActivityTable/data/hooks/useCourseEnrollments.js
@@ -1,10 +1,13 @@
 import usePaginatedTableData from '../../../../hooks/usePaginatedTableData';
 import EnterpriseDataApiService from '../../../../data/services/EnterpriseDataApiService';
 
-const useCourseEnrollments = (enterpriseId, tableId, apiFieldsForColumnAccessor) => usePaginatedTableData({
+const useCourseEnrollments = (enterpriseId, tableId, apiFieldsForColumnAccessor, activity) => usePaginatedTableData({
   enterpriseId,
   tableId,
   apiFieldsForColumnAccessor,
+  fetchFunctionOptions: {
+    learnerActivity: activity,
+  },
   fetchFunction: EnterpriseDataApiService.fetchCourseEnrollments,
 });
 
