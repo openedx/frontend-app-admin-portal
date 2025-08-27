1c569d9d4158e3ad2078294e9295c5069fd60fcb
Hamzah Ullah <hamzahullah@yahoo.com>
2025-08-27 13:26:20 -0400

    chore: remove paragon types file

--- src/types/paragon.d.ts
+++
@@ -1,8 +0,0 @@
-import { AlertHeadingProps as OriginalAlertHeadingProps } from '@openedx/paragon';
-
-declare module '@openedx/paragon' {
-  // Extend the AlertHeadingProps interface to include className
-  export interface AlertHeadingProps extends OriginalAlertHeadingProps {
-    className?: string;
-  }
-}
