0b2b8b9c6a4dcfe5e5ccd01d90b29e7f164ca2a3
Jono Booth <jono@puzzl.co.za>
2025-09-11 09:55:24 +0200

    feat: ENT-10827 Add start date to spent table (#1664)

--- src/components/learner-credit-management/LearnerCreditAllocationTable.jsx
+++ src/components/learner-credit-management/LearnerCreditAllocationTable.jsx
@@ -48,6 +48,20 @@ const LearnerCreditAllocationTable = ({
         {
           Header:
           intl.formatMessage({
+            id: 'lcm.learner.credit.allocation.spent.table.column.startDate',
+            defaultMessage: 'Start Date',
+            description: 'Column header for the Course Run Start Date column in the Learner Credit Allocation table',
+          }),
+          accessor: 'courseRunStartDate',
+          Cell: ({ row }) => {
+            const value = row.values.courseRunStartDate;
+            return value ? formatDate(value) : null;
+          },
+          disableFilters: true,
+        },
+        {
+          Header:
+          intl.formatMessage({
             id: 'lcm.learner.credit.allocation.spent.table.column.enrollment.details',
             defaultMessage: 'Enrollment details',
             description: 'Column header for the Enrollment details column in the Learner Credit Allocation table',
